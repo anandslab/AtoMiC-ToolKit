a4a559adb8271b76c1ed21fdfdf50b048ee62b91
Anand Subramanian <htpcbeginner@gmail.com>
2015-03-15 16:21:22 -0400

    Create setup.sh
    AtoMiC (Automated Media Center) Setup Script

---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Script Name: AtoMiC ToolKit
+# Author: htpcBeginner.com
+# Publisher: http://www.htpcBeginner.com
+# Version: 1.0 (March 15, 2015) - Initial Release
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
+YELLOW='\e[93m'
+RED='\e[91m'
+ENDCOLOR='\033[0m'
+CYAN='\e[96m'
+GREEN='\e[92m'
+SCRIPTPATH=$(pwd)
+
+clear
+echo 
+echo -e $RED
+echo -e " ┬ ┬┬ ┬┬ ┬ ┬ ┬┌┬┐┌─┐┌─┐┌┐ ┌─┐┌─┐┬┌┐┌┌┐┌┌─┐┬─┐ ┌─┐┌─┐┌┬┐"
+echo -e " │││││││││ ├─┤ │ ├─┘│  ├┴┐├┤ │ ┬│││││││├┤ ├┬┘ │  │ ││││"
+echo -e " └┴┘└┴┘└┴┘o┴ ┴ ┴ ┴  └─┘└─┘└─┘└─┘┴┘└┘┘└┘└─┘┴└─o└─┘└─┘┴ ┴"
+echo -e $CYAN
+echo -e "                __  ___             "
+echo -e "  /\ |_ _ |\/|./     | _  _ ||_/.|_ "
+echo -e " /--\|_(_)|  ||\__   |(_)(_)|| \||_ "
+echo
+echo -e $GREEN'AtoMiC ToolKit HTPC / Home Server Setup Script'$ENDCOLOR
+echo 

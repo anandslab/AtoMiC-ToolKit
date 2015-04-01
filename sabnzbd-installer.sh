5c6cb10a7015783b4a56e4e4b96893c14134dbe4
htpcBeginner <admin@htpcbeginner.com>
2015-03-31 20:31:28 -0400

    updates

--- sabnzbd-installer.sh
+++ sabnzbd-installer.sh
@@ -68,12 +68,62 @@ if [ ! -d "/home/$UNAME" ] || [ -z "$UNAME" ]; then
 fi
 UGROUP=($(id -gn $UNAME))
 
-sudo add-apt-repository ppa:jcfp/ppa
-sudo apt-get update
+echo
+sleep 1
 
-sudo nano /etc/default/sabnzbdplus
-USER=$UNAME
-For remote access:
-HOST=0.0.0.0
-sudo service sabnzbdplus restart
+echo -e $YELLOW"--->Adding Transmission repository..."$ENDCOLOR
+GREPOUT=$(grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep jcfp)
+if [ "$GREPOUT" == "" ]; then
+    sudo add-apt-repository -y ppa:jcfp/ppa
+else
+    echo "SABnzbd PPA repository already exists..."
+fi
 
+echo
+sleep 1
+
+echo -e $YELLOW"--->Refreshing packages list again..."$ENDCOLOR
+#sudo apt-get update
+
+echo
+sleep 1
+
+echo -e $YELLOW"--->Installing SABnzbd..."$ENDCOLOR
+sudo apt-get -y install sabnzbdplus
+
+echo 
+sleep 1
+
+echo -e $YELLOW"--->Making some configuration changes..."$ENDCOLOR
+sudo sed -i 's/USER=/USER='$UNAME'/g' /etc/default/sabnzbdplus  || { echo -e $RED'Replacing username in default failed.'$ENDCOLOR ; exit 1; }
+sudo sed -i 's/HOST=/HOST=0.0.0.0/g' /etc/default/sabnzbdplus  || { echo -e $RED'Replacing host in default failed.'$ENDCOLOR ; exit 1; }
+
+echo 
+sleep 1
+
+echo -e $YELLOW"--->Enabling autostart during boot..."$ENDCOLOR
+sudo update-rc.d sabnzbdplus defaults  >/dev/null 2>&1
+
+echo 
+sleep 1
+
+echo -e $YELLOW"--->Starting SABnzbd..."$ENDCOLOR
+sudo service sabnzbdplus start >/dev/null 2>&1
+
+sleep 1
+
+echo
+echo -e $GREEN'--->All done. '$ENDCOLOR
+echo -e 'SABnzbd should start within 10-20 seconds.'
+echo -e 'If not you can start it using '$CYAN'sudo service sabnzbdplus start'$ENDCOLOR' command.'
+echo -e 'Then open '$CYAN'http://localhost:8080'$ENDCOLOR' in your browser.'
+echo
+echo -e $YELLOW'If this script worked for you, please visit '$CYAN'http://www.htpcBeginner.com'$YELLOW' and like/follow us.'$ENDCOLOR
+echo -e $YELLOW'Thank you for using the AtoMiC Sonarr Install script from www.htpcBeginner.com.'$ENDCOLOR 
+echo
+
+pause 'Press [Enter] key to continue...'
+
+cd $SCRIPTPATH
+#sudo ./setup.sh
+exit 0
---
+++ sabnzbd-uninstaller.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+# Script Name: AtoMiC SABnzbd Uninstaller
+# Author: htpcBeginner
+# Publisher: http://www.htpcBeginner.com
+# License: MIT License (refer to README.md for more details)
+#
+
+# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
+YELLOW='\e[93m'
+RED='\e[91m'
+ENDCOLOR='\033[0m'
+CYAN='\e[96m'
+GREEN='\e[92m'
+SCRIPTPATH=$(pwd)
+
+function pause(){
+   read -p "$*"
+}
+
+clear
+echo 
+echo -e $RED
+echo -e " ┬ ┬┬ ┬┬ ┬ ┬ ┬┌┬┐┌─┐┌─┐┌┐ ┌─┐┌─┐┬┌┐┌┌┐┌┌─┐┬─┐ ┌─┐┌─┐┌┬┐"
+echo -e " │││││││││ ├─┤ │ ├─┘│  ├┴┐├┤ │ ┬│││││││├┤ ├┬┘ │  │ ││││"
+echo -e " └┴┘└┴┘└┴┘o┴ ┴ ┴ ┴  └─┘└─┘└─┘└─┘┴┘└┘┘└┘└─┘┴└─o└─┘└─┘┴ ┴"
+echo -e $CYAN
+echo -e "                __     __           "
+echo -e "  /\ |_ _ |\/|./      (_  _ _. _ |_ "
+echo -e " /--\|_(_)|  ||\__    __)(_| ||_)|_ "
+echo -e "                              |     "
+echo -e $GREEN'AtoMiC SABnzbd Uninstaller Script'$ENDCOLOR
+echo 
+echo -e $YELLOW'--->SABnzbd uninstallation will start soon. Please read the following carefully.'$ENDCOLOR
+
+echo -e '1. The script has been confirmed to work on Ubuntu variants, Mint, and Ubuntu Server.'
+echo -e '2. While several testing runs identified no known issues, '$CYAN'www.htpcBeginner.com'$ENDCOLOR' or the authors cannot be held accountable for any problems that might occur due to the script.'
+echo -e '3. If you did not run this script with sudo, you maybe asked for your root password during installation.'
+echo -e '4. By proceeding you authorize this script to uninstall any relevant files/packages.'
+echo -e '5. Best used to uninstall SABnzbd installed using AtoMiC ToolKit or '$CYAN'www.htpcBeginner.com'$ENDCOLOR' guides.'
+
+echo
+
+read -p 'Type y/Y and press [ENTER] to AGREE and continue with the installation or any other key to exit: '
+RESP=${REPLY,,}
+
+if [ "$RESP" != "y" ] 
+then
+	echo -e $RED'So you chickened out. May be you will try again later.'$ENDCOLOR
+	echo
+	pause 'Press [Enter] key to continue...'
+	cd $SCRIPTPATH
+	sudo ./setup.sh
+	exit 0
+fi
+
+echo 
+
+echo -n 'Type the username of the user you installed SickBeard as and press [ENTER]. Typically, this is your system login name (IMPORTANT! Ensure correct spelling and case): '
+read UNAME
+
+if [ ! -d "/home/$UNAME" ] || [ -z "$UNAME" ]; then
+  echo -e $RED'Bummer! You may not have entered your username correctly. Exiting now. Please rerun script.'$ENDCOLOR
+	echo
+	pause 'Press [Enter] key to continue...'
+	cd $SCRIPTPATH
+	sudo ./setup.sh
+	exit 0
+fi
+
+echo
+sleep 1
+
+echo -e $YELLOW'--->Stopping SABnzbd...'$ENDCOLOR
+sudo service sabnzbdplus stop >/dev/null 2>&1
+sudo killall sabnzbdplus >/dev/null 2>&1
+
+echo
+sleep 1
+
+echo -e $YELLOW'--->Uninstalling SABnzbd...'$ENDCOLOR
+sudo apt-get -y remove sabnzbdplus*
+
+echo
+sleep 1
+
+echo -e $YELLOW'--->Removing SABnzbd Autostart scripts...'$ENDCOLOR
+sudo update-rc.d -f sabnzbdplus remove || { echo -e $RED'Warning! update-rc.d remove failed.'$ENDCOLOR ; }
+sudo rm /etc/default/sabnzbdplus || { echo -e $RED'Warning! Removing default script failed.'$ENDCOLOR ; }
+sudo rm /etc/init.d/sabnzbdplus || { echo -e $RED'Warning! Removing init script failed.'$ENDCOLOR ; }
+
+echo
+sleep 1
+
+read -p 'You may keep SABnzbd configuration files in case you want to reinstall later. Do you want to keep the files? Type y/Y and press [ENTER]: '
+FILEDEL=${REPLY,,}
+
+if [ "$FILEDEL" != "y" ] 
+then
+	echo
+	echo -e $YELLOW'--->Deleting SABnzbd files from '$CYAN'/home/'$UNAME'/.sabnzbd'$YELLOW'...'$ENDCOLOR
+	sudo rm -r /home/$UNAME/.sabnzbd >/dev/null 2>&1
+else
+	echo
+	echo -e $YELLOW'--->Keeping SABnzbd files in '$CYAN'/home/'$UNAME'/.sabnzbd'$YELLOW'...'$ENDCOLOR
+fi
+
+echo
+sleep 1
+
+echo -e $GREEN'--->All done.'$ENDCOLOR
+echo -e 'SABnzbd Uninstalled.'
+echo
+echo -e $YELLOW'If this script worked for you, please visit '$CYAN'http://www.htpcBeginner.com'$YELLOW' and like/follow us.'$ENDCOLOR
+echo -e $YELLOW'Thank you for using the AtoMiC SABnzbd Uninstall script from www.htpcBeginner.com.'$ENDCOLOR 
+echo
+
+pause 'Press [Enter] key to continue...'
+
+cd $SCRIPTPATH
+sudo ./setup.sh
+exit 0

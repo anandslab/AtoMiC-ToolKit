e5e0e152d80c370e42193c0fcb04b4e6c253eb19
Anand Subramanian <htpcbeginner@gmail.com>
2015-05-04 14:42:28 -0400

    Create default-credentials.sh

---
+++ default-credentials.sh
@@ -0,0 +1,39 @@
+Sick Beard
+Port: 8081
+Default Username: none
+Default Password: none
+
+SickRage
+Port: 8081
+Default Username: none
+Default Password: none
+
+SickGear
+Port: 8081
+Default Username: none
+Default Password: none
+
+Sonarr
+Port: 8989
+Default Username: none
+Default Password: none
+
+CouchPotato
+Port: 5050
+Default Username: none
+Default Password: none
+
+Transmission
+Port: 9091
+Default Username: transmission
+Default Password: transmission
+
+qBittorrent
+Port: 8085
+Default Username: admin
+Default Password: adminadmin
+
+SABNzbd
+Port: 8080
+Default Username: none
+Default Password: none

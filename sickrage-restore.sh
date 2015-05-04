5299fabe2140925e032490f5ffbf339b5b8fa29f
Anand Subramanian <htpcbeginner@gmail.com>
2015-05-03 20:48:11 -0400

    Update sickrage-restore.sh

--- sickrage-restore.sh
+++ sickrage-restore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script Name: AtoMiC Sick Beard Restore Script
+# Script Name: AtoMiC SickRage Restore Script
 # Author: htpcBeginner
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)
@@ -28,15 +28,15 @@ echo -e "                __     __           "
 echo -e "  /\ |_ _ |\/|./      (_  _ _. _ |_ "
 echo -e " /--\|_(_)|  ||\__    __)(_| ||_)|_ "
 echo -e "                              |     "
-echo -e $GREEN'AtoMiC Sick Beard Restore Script'$ENDCOLOR
+echo -e $GREEN'AtoMiC SickRage Restore Script'$ENDCOLOR
 echo 
-echo -e $YELLOW'--->Sick Beard backup will start soon. Please read the following carefully.'$ENDCOLOR
+echo -e $YELLOW'--->SickRage backup will start soon. Please read the following carefully.'$ENDCOLOR
 
 echo -e '1. The script has been confirmed to work on Ubuntu variants, Mint, and Ubuntu Server.'
 echo -e '2. While several testing runs identified no known issues, '$CYAN'www.htpcBeginner.com'$ENDCOLOR' or the authors cannot be held accountable for any problems that might occur due to the script.'
 echo -e '3. If you did not run this script with sudo, you maybe asked for your root password during installation.'
-echo -e '4. Best used to restore Sick Beard backed up using AtoMiC ToolKit or '$CYAN'www.htpcBeginner.com'$ENDCOLOR' guides.'
-echo -e '5. Sick Beard should already be installed before restoring library and settings.'
+echo -e '4. Best used to restore SickRage backed up using AtoMiC ToolKit or '$CYAN'www.htpcBeginner.com'$ENDCOLOR' guides.'
+echo -e '5. SickRage should already be installed before restoring library and settings.'
 
 echo
 
@@ -55,7 +55,7 @@ fi
 
 echo 
 
-echo -n 'Type the username of the user you run Sick Beard as and press [ENTER]. Typically, this is your system login name (IMPORTANT! Ensure correct spelling and case): '
+echo -n 'Type the username of the user you run SickRage as and press [ENTER]. Typically, this is your system login name (IMPORTANT! Ensure correct spelling and case): '
 read UNAME
 
 if [ ! -d "/home/$UNAME" ] || [ -z "$UNAME" ]; then
@@ -70,9 +70,9 @@ UGROUP=($(id -gn $UNAME))
 
 echo
 
-if [ ! -d "/home/$UNAME/.sickbeard" ]; 
+if [ ! -d "/home/$UNAME/.sickrage" ]; 
 then
-	echo -e $RED'Error! '$CYAN'/home/'$UNAME/'.sickbeard'$RED' not found. Ensure that Sick Beard is installed. Exiting now. Please rerun script.'$ENDCOLOR
+	echo -e $RED'Error! '$CYAN'/home/'$UNAME/'.sickrage'$RED' not found. Ensure that SickRage is installed. Exiting now. Please rerun script.'$ENDCOLOR
 	echo
 	pause 'Press [Enter] key to continue...'
 	cd $SCRIPTPATH
@@ -83,42 +83,42 @@ fi
 echo -e $YELLOW'--->Checking for existing files...'$ENDCOLOR
 sleep 1
 cd /home/$UNAME
-if [ -f "/home/$UNAME/.sickbeard/sickbeard.db" ]; then
-	mv /home/$UNAME/.sickbeard/sickbeard.db /home/$UNAME/.sickbeard/sickbeard.db_previous
-	echo -e 'Moved existing sickbeard.db to '$CYAN'/home/'$UNAME'/.sickbeard/sickbeard.db_previous'$ENDCOLOR
+if [ -f "/home/$UNAME/.sickrage/sickbeard.db" ]; then
+	mv /home/$UNAME/.sickrage/sickbeard.db /home/$UNAME/.sickrage/sickbeard.db_previous
+	echo -e 'Moved existing sickbeard.db to '$CYAN'/home/'$UNAME'/.sickrage/sickbeard.db_previous'$ENDCOLOR
 fi
 
-if [ -f "/home/$UNAME/.sickbeard/cache.db" ]; then
-	mv /home/$UNAME/.sickbeard/cache.db /home/$UNAME/.sickbeard/cache.db_previous
-	echo -e 'Moved existing cache.db to '$CYAN'/home/'$UNAME'/.sickbeard/cache.db_previous'$ENDCOLOR
+if [ -f "/home/$UNAME/.sickrage/cache.db" ]; then
+	mv /home/$UNAME/.sickrage/cache.db /home/$UNAME/.sickrage/cache.db_previous
+	echo -e 'Moved existing cache.db to '$CYAN'/home/'$UNAME'/.sickrage/cache.db_previous'$ENDCOLOR
 fi
 
-if [ -f "/home/$UNAME/.sickbeard/failed.db" ]; then
-	mv /home/$UNAME/.sickbeard/failed.db /home/$UNAME/.sickbeard/failed.db_previous
-	echo -e 'Moved existing failed.db to '$CYAN'/home/'$UNAME'/.sickbeard/failed.db_previous'$ENDCOLOR
+if [ -f "/home/$UNAME/.sickrage/failed.db" ]; then
+	mv /home/$UNAME/.sickrage/failed.db /home/$UNAME/.sickrage/failed.db_previous
+	echo -e 'Moved existing failed.db to '$CYAN'/home/'$UNAME'/.sickrage/failed.db_previous'$ENDCOLOR
 fi
 
-if [ -f "/home/$UNAME/.sickbeard/config.ini" ]; then
-	mv /home/$UNAME/.sickbeard/config.ini /home/$UNAME/.sickbeard/config.ini_previous
-	echo -e 'Moved existing config.ini to '$CYAN'/home/'$UNAME'/.sickbeard/config.ini_previous'$ENDCOLOR
+if [ -f "/home/$UNAME/.sickrage/config.ini" ]; then
+	mv /home/$UNAME/.sickrage/config.ini /home/$UNAME/.sickrage/config.ini_previous
+	echo -e 'Moved existing config.ini to '$CYAN'/home/'$UNAME'/.sickrage/config.ini_previous'$ENDCOLOR
 fi
 
-if [ -f "/home/$UNAME/.sickbeard/autoProcessTV/autoProcessTV.cfg" ]; then
-	mv /home/$UNAME/.sickbeard/autoProcessTV/autoProcessTV.cfg /home/$UNAME/.sickbeard/autoProcessTV/autoProcessTV.cfg_previous
-	echo -e 'Moved existing autoProcessTV.cfg to '$CYAN'/home/'$UNAME'/.sickbeard/autoProcessTV/autoProcessTV.cfg_previous'$ENDCOLOR
+if [ -f "/home/$UNAME/.sickrage/autoProcessTV/autoProcessTV.cfg" ]; then
+	mv /home/$UNAME/.sickrage/autoProcessTV/autoProcessTV.cfg /home/$UNAME/.sickrage/autoProcessTV/autoProcessTV.cfg_previous
+	echo -e 'Moved existing autoProcessTV.cfg to '$CYAN'/home/'$UNAME'/.sickrage/autoProcessTV/autoProcessTV.cfg_previous'$ENDCOLOR
 fi
 
 echo
 sleep 1
 
-echo -e $YELLOW'--->Select Sick Beard backup file to restore...'$ENDCOLOR
+echo -e $YELLOW'--->Select SickRage backup file to restore...'$ENDCOLOR
 echo -e 'In the following file selection box use ARROW or TAB keys to move and SPACE key to select the backup file (with extension '$CYAN'tar.gz'$ENDCOLOR') to restore.'
 
 echo
 
 pause 'Press [Enter] key to continue...'
 
-BFILE=$(dialog --title "Restore Sick Beard" --stdout --title "Choose backup file to restore. Use TAB or ARROW keys to move and SPACE to select." --fselect /home/$UNAME/ 15 120)
+BFILE=$(dialog --title "Restore SickRage" --stdout --title "Choose backup file to restore. Use TAB or ARROW keys to move and SPACE to select." --fselect /home/$UNAME/ 15 120)
 
 if [ -f $BFILE ] 
 then
@@ -145,10 +145,10 @@ fi
 
 echo
 echo -e $GREEN'--->All done. '$ENDCOLOR
-echo -e 'Sick Beard files restored.'
+echo -e 'SickRage files restored.'
 echo
 echo -e $YELLOW'If this script worked for you, please visit '$CYAN'http://www.htpcBeginner.com'$YELLOW' and like/follow us.'$ENDCOLOR
-echo -e $YELLOW'Thank you for using the AtoMiC Sick Beard Restore script from www.htpcBeginner.com.'$ENDCOLOR 
+echo -e $YELLOW'Thank you for using the AtoMiC SickRage Restore script from www.htpcBeginner.com.'$ENDCOLOR 
 echo
 
 pause 'Press [Enter] key to continue...'

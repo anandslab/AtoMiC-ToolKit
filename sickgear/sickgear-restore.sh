ff19c484ce3f758298445bb2ad7ab2389b767f4e
htpcBeginner <admin@htpcbeginner.com>
2016-01-18 18:33:04 -0500

    updates

--- sickgear/sickgear-restore.sh
+++ sickgear/sickgear-restore.sh
@@ -6,78 +6,26 @@
 #
 
 # DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
-YELLOW='\e[93m'
-RED='\e[91m'
-ENDCOLOR='\033[0m'
-CYAN='\e[96m'
-GREEN='\e[92m'
-SCRIPTPATH=$(pwd)
 
-function pause(){
-   read -p "$*"
-}
-
-clear
-echo 
-echo -e $RED
-echo -e " ┬ ┬┬ ┬┬ ┬ ┬ ┬┌┬┐┌─┐┌─┐┌┐ ┌─┐┌─┐┬┌┐┌┌┐┌┌─┐┬─┐ ┌─┐┌─┐┌┬┐"
-echo -e " │││││││││ ├─┤ │ ├─┘│  ├┴┐├┤ │ ┬│││││││├┤ ├┬┘ │  │ ││││"
-echo -e " └┴┘└┴┘└┴┘o┴ ┴ ┴ ┴  └─┘└─┘└─┘└─┘┴┘└┘┘└┘└─┘┴└─o└─┘└─┘┴ ┴"
-echo -e $CYAN
-echo -e "                __  ___             "
-echo -e "  /\ |_ _ |\/|./     | _  _ ||_/.|_ "
-echo -e " /--\|_(_)|  ||\__   |(_)(_)|| \||_ "
-echo
-echo -e $GREEN'AtoMiC SickGear Restore Script'$ENDCOLOR
-echo 
-echo -e $YELLOW'--->SickGear backup will start soon. Please read the following carefully.'$ENDCOLOR
-
-echo -e '1. The script has been confirmed to work on Ubuntu variants, Mint, and Ubuntu Server.'
-echo -e '2. While several testing runs identified no known issues, '$CYAN'www.htpcBeginner.com'$ENDCOLOR' or the authors cannot be held accountable for any problems that might occur due to the script.'
-echo -e '3. If you did not run this script with sudo, you maybe asked for your root password during installation.'
-echo -e '4. Best used to restore SickGear backed up using AtoMiC ToolKit or '$CYAN'www.htpcBeginner.com'$ENDCOLOR' guides.'
-echo -e '5. SickGear should already be installed before restoring library and settings.'
-
-echo
-
-read -p 'Type y/Y and press [ENTER] to AGREE and continue with the installation or any other key to exit: '
-RESP=${REPLY,,}
-
-if [ "$RESP" != "y" ] 
+if [[ $1 != *"setup.sh"* ]]
 then
-	echo -e $RED'So you chickened out. May be you will try again later.'$ENDCOLOR
-	echo
-	pause 'Press [Enter] key to continue...'
-	cd $SCRIPTPATH
-	sudo bash setup.sh
-	exit 0
+  echo
+  echo -e '\e[91mCannot be run directly. Please run setup.sh from AtoMiC ToolKit root folder: \033[0msudo bash setup.sh'
+  echo
+  exit 0
 fi
 
-echo 
+source $2/inc/commons.sh
+source $SCRIPTPATH/inc/header.sh
 
-echo -n 'Type the username of the user you run SickGear as and press [ENTER]. Typically, this is your system login name (IMPORTANT! Ensure correct spelling and case): '
-read UNAME
+echo -e $GREEN'AtoMiC SickGear Restore Script'$ENDCOLOR
 
-if [ ! -d "/home/$UNAME" ] || [ -z "$UNAME" ]; then
-	echo -e $RED'Bummer! You may not have entered your username correctly. Exiting now. Please rerun script.'$ENDCOLOR
-	echo
-	pause 'Press [Enter] key to continue...'
-	cd $SCRIPTPATH
-	sudo bash setup.sh
-	exit 0
-fi
-UGROUP=($(id -gn $UNAME))
-
-echo
+source $SCRIPTPATH/inc/pause.sh
 
 if [ ! -d "/home/$UNAME/.sickgear" ]; 
 then
 	echo -e $RED'Error! '$CYAN'/home/'$UNAME/'.sickgear'$RED' not found. Ensure that SickGear is installed. Exiting now. Please rerun script.'$ENDCOLOR
-	echo
-	pause 'Press [Enter] key to continue...'
-	cd $SCRIPTPATH
-	sudo bash setup.sh
-	exit 0
+	source $SCRIPTPATH/inc/exit.sh
 fi
 
 echo
@@ -124,7 +72,7 @@ echo -e 'In the following file selection box use ARROW or TAB keys to move and S
 
 echo
 
-pause 'Press [Enter] key to continue...'
+source $SCRIPTPATH/inc/pause.sh
 
 echo -e $YELLOW'--->Installing necessary '$CYAN'dialog'$YELLOW' package...'$ENDCOLOR
 sudo apt-get -y install dialog
@@ -142,31 +90,16 @@ then
 		/etc/init.d/sickgear start
 	else
 		echo -e $RED'Error! Selected file is not a backup file with '$CYAN'tar.gz'$RED' extension. Exiting now. Please rerun script.'$ENDCOLOR
-		echo
-		pause 'Press [Enter] key to continue...'
-		cd $SCRIPTPATH
-		sudo bash setup.sh
-		exit 0
+		source $SCRIPTPATH/inc/exit.sh
 	fi
 else
 	echo -e $RED'Error! No file selected. Exiting now. Please rerun script.'$ENDCOLOR
-	echo
-	pause 'Press [Enter] key to continue...'
-	cd $SCRIPTPATH
-	sudo bash setup.sh
-	exit 0
+	source $SCRIPTPATH/inc/exit.sh
 fi 
 
 echo
 echo -e $GREEN'--->All done. '$ENDCOLOR
 echo -e 'SickGear files restored.'
-echo
-echo -e $YELLOW'If this script worked for you, please visit '$CYAN'http://www.htpcBeginner.com'$YELLOW' and like/follow us.'$ENDCOLOR
-echo -e $YELLOW'Thank you for using the AtoMiC SickGear Restore script from www.htpcBeginner.com.'$ENDCOLOR 
-echo
 
-pause 'Press [Enter] key to continue...'
-
-cd $SCRIPTPATH
-sudo bash setup.sh
-exit 0
+source $SCRIPTPATH/inc/thankyou.sh
+source $SCRIPTPATH/inc/exit.sh
--- sickrage/sickrage-backup.sh
+++ sickrage/sickrage-backup.sh
@@ -6,85 +6,37 @@
 #
 
 # DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
-YELLOW='\e[93m'
-RED='\e[91m'
-ENDCOLOR='\033[0m'
-CYAN='\e[96m'
-GREEN='\e[92m'
-SCRIPTPATH=$(pwd)
 
-function pause(){
-   read -p "$*"
-}
-
-clear
-echo 
-echo -e $RED
-echo -e " ┬ ┬┬ ┬┬ ┬ ┬ ┬┌┬┐┌─┐┌─┐┌┐ ┌─┐┌─┐┬┌┐┌┌┐┌┌─┐┬─┐ ┌─┐┌─┐┌┬┐"
-echo -e " │││││││││ ├─┤ │ ├─┘│  ├┴┐├┤ │ ┬│││││││├┤ ├┬┘ │  │ ││││"
-echo -e " └┴┘└┴┘└┴┘o┴ ┴ ┴ ┴  └─┘└─┘└─┘└─┘┴┘└┘┘└┘└─┘┴└─o└─┘└─┘┴ ┴"
-echo -e $CYAN
-echo -e "                __  ___             "
-echo -e "  /\ |_ _ |\/|./     | _  _ ||_/.|_ "
-echo -e " /--\|_(_)|  ||\__   |(_)(_)|| \||_ "
-echo
-echo -e $GREEN'AtoMiC SickRage Backup Script'$ENDCOLOR
-echo 
-echo -e $YELLOW'--->SickRage backup will start soon. Please read the following carefully.'$ENDCOLOR
-
-echo -e '1. The script has been confirmed to work on Ubuntu variants, Mint, and Ubuntu Server.'
-echo -e '2. While several testing runs identified no known issues, '$CYAN'www.htpcBeginner.com'$ENDCOLOR' or the authors cannot be held accountable for any problems that might occur due to the script.'
-echo -e '3. If you did not run this script with sudo, you maybe asked for your root password during installation.'
-echo -e '4. Best used to backup SickRage installed using AtoMiC ToolKit or '$CYAN'www.htpcBeginner.com'$ENDCOLOR' guides.'
-
-echo
-
-read -p 'Type y/Y and press [ENTER] to AGREE and continue with the installation or any other key to exit: '
-RESP=${REPLY,,}
-
-if [ "$RESP" != "y" ] 
+if [[ $1 != *"setup.sh"* ]]
 then
-	echo -e $RED'So you chickened out. May be you will try again later.'$ENDCOLOR
-	echo
-	pause 'Press [Enter] key to continue...'
-	cd $SCRIPTPATH
-	sudo bash setup.sh
-	exit 0
+  echo
+  echo -e '\e[91mCannot be run directly. Please run setup.sh from AtoMiC ToolKit root folder: \033[0msudo bash setup.sh'
+  echo
+  exit 0
 fi
 
-echo 
+source $2/inc/commons.sh
+source $SCRIPTPATH/inc/header.sh
 
-echo -n 'Type the username of the user you run SickRage as and press [ENTER]. Typically, this is your system login name (IMPORTANT! Ensure correct spelling and case): '
-read UNAME
+echo -e $GREEN'AtoMiC SickRage Backup Script'$ENDCOLOR
 
-if [ ! -d "/home/$UNAME" ] || [ -z "$UNAME" ]; then
-	echo -e $RED'Bummer! You may not have entered your username correctly. Exiting now. Please rerun script.'$ENDCOLOR
-	echo
-	pause 'Press [Enter] key to continue...'
-	cd $SCRIPTPATH
-	sudo bash setup.sh
-	exit 0
-fi
-UGROUP=($(id -gn $UNAME))
-
-echo
-sleep 1
+source $SCRIPTPATH/inc/pause.sh
 
 echo -e $YELLOW'--->Creating a list of files to backup...'$ENDCOLOR
 cd $SCRIPTPATH
-cp sickrage-backup-files sr-backup-files
-sudo sed -i 's/UNAME/'$UNAME'/g' sr-backup-files  || { echo -e $RED'Replacing username in backup list failed.'$ENDCOLOR ; exit 1; }
+cp $SCRIPTPATH/sickrage/sickrage-backup-files $SCRIPTPATH/tmp/sr-backup-files
+sudo sed -i 's/UNAME/'$UNAME'/g' $SCRIPTPATH/tmp/sr-backup-files  || { echo -e $RED'Replacing username in backup list failed.'$ENDCOLOR ; exit 1; }
 
 echo
 sleep 1
 
 echo -e $YELLOW'--->Backing up files...'$ENDCOLOR
 BFN=sickrage_`date '+%m-%d-%Y_%H-%M'`
-tar -zcvf $BFN.tar.gz --files-from sr-backup-files || { echo -e $RED'Creating tar file failed.'$ENDCOLOR ; exit 1; }
+tar -zcvf $BFN.tar.gz --files-from $SCRIPTPATH/tmp/sr-backup-files || { echo -e $RED'Creating tar file failed.'$ENDCOLOR ; exit 1; }
 echo
 echo -e "Following files were backed up:"
-cat sr-backup-files
-rm sr-backup-files
+cat $SCRIPTPATH/tmp/sr-backup-files
+rm $SCRIPTPATH/tmp/sr-backup-files
 
 echo
 sleep 1
@@ -98,16 +50,6 @@ sleep 1
 echo
 echo -e $GREEN'--->All done. '$ENDCOLOR
 echo -e 'SickRage files backed up. You can use the restore utility to restore it on a new computer.'
-echo
-echo -e $YELLOW'If this script worked for you, please visit '$CYAN'http://www.htpcBeginner.com'$YELLOW' and like/follow us.'$ENDCOLOR
-echo -e $YELLOW'Thank you for using the AtoMiC SickRage Backup script from www.htpcBeginner.com.'$ENDCOLOR 
-echo
 
-pause 'Press [Enter] key to continue...'
-
-cd $SCRIPTPATH
-sudo bash setup.sh
-exit 0
-
-
-
+source $SCRIPTPATH/inc/thankyou.sh
+source $SCRIPTPATH/inc/exit.sh

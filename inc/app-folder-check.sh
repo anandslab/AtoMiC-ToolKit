a358dc00e33c4f9d0c38c2c6a34fef7e0b4b71e2
htpcbeginner <atomic@htpcbeginner.com>
2016-01-29 13:58:56 -0500

    updates

--- inc/app-folder-check.sh
+++ inc/app-folder-check.sh
@@ -3,7 +3,7 @@ if [ ! -d "$APPPATH" ];
 then
 	echo -e $RED'Error! '$CYAN$APPPATH$RED' not found. '$APPTITLE' not installed or incompatible installation.'$ENDCOLOR
     cd $SCRIPTPATH
-    echo 'Exiting. Use '$CYANsudo ./setup.sh$ENDCOLOR' to start again.'
+    echo 'Exiting. If '$APPTITLE' is installed, use sudo ./setup.sh to start it.'
     echo
     exit 1;
 fi
--- inc/app-settings-check.sh
+++ inc/app-settings-check.sh
@@ -3,7 +3,7 @@ if [ ! -f "$APPSETTINGS" ];
 then
 	echo -e $RED'Error! '$CYAN$APPSETTINGS$RED' not found. '$APPTITLE' not installed or incompatible installation.'$ENDCOLOR
     cd $SCRIPTPATH
-    echo 'Exiting. Use '$CYAN'sudo ./setup.sh'$ENDCOLOR' to start again.'
+    echo 'Exiting. If '$APPTITLE' is installed, use sudo ./setup.sh to start it.'
     echo
     exit 1;
 fi

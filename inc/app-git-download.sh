ac0f4279a8832726d9646dba4ac8344c4d64ab62
Andre Greeff <little.grief@gmail.com>
2016-08-14 21:04:19 +0200

    restrict git clone command to a commit history depth of 1

--- inc/app-git-download.sh
+++ inc/app-git-download.sh
@@ -1,5 +1,5 @@
 echo
 sleep 1
 echo -e $YELLOW'--->Downloading latest '$APPTITLE'...'$ENDCOLOR
-git clone $APPGIT $APPPATH || { echo -e $RED'Git not found.'$ENDCOLOR ; exit 1; }
+git clone --depth 1 $APPGIT $APPPATH || { echo -e $RED'Git not found.'$ENDCOLOR ; exit 1; }
 source $SCRIPTPATH/inc/app-set-permissions.sh

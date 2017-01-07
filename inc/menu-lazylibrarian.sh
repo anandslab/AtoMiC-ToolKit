687a299e348e251fe912279e4af892bec1687dd6
TommyE123 <Tom.Eley@gmail.com>
2017-01-07 09:58:04 +0000

    Minor Lazy Librarian code tidy up
    Fixed new lines on end of files and adjusted git location

--- inc/menu-lazylibrarian.sh
+++ inc/menu-lazylibrarian.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# Script Name: AtoMiC Lazy Librarian Restore Script
+# Author: TommyE123
+# Publisher: http://www.htpcBeginner.com
+# License: MIT License (refer to README.md for more details)
+#
+
+# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
+
 if [[ $ISSETUP != "Yes" ]]
 then
   echo

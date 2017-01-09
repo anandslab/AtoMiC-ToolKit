3eeb48a9c3c9bf37337835889a3f36299408bb4b
TommyE123 <Tom.Eley@gmail.com>
2017-01-09 19:33:26 +0000

    Lazy Librarian minor update to permissions on systemD file

--- lazylibrarian/lazylibrarian-systemd-update.sh
+++ lazylibrarian/lazylibrarian-systemd-update.sh
@@ -7,7 +7,6 @@
 
 # DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
 
-sudo chmod 644 /etc/systemd/system/$APPSYSTEMD
 sudo sed -i "s@ExecStart=/usr/bin/python /home/lazylibrarian/LazyLibrarian.py --daemon --config /home/lazylibrarian/lazylibrarian.ini --datadir /home/lazylibrarian/.lazylibrarian --nolaunch@ExecStart=/usr/bin/python $APPPATH/LazyLibrarian.py --daemon --config $APPPATH/lazylibrarian.ini --datadir $APPPATH/.lazylibrarian --nolaunch@g" /etc/systemd/system/$APPSYSTEMD  || { echo -e $RED'Modifying ExecStart in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
 sudo sed -i "s@User=lazylibrarian@User=$UNAME@g" /etc/systemd/system/$APPSYSTEMD  || { echo -e $RED'Modifying USER in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
 sudo sed -i "s@Group=lazylibrarian@Group=$UGROUP@g" /etc/systemd/system/$APPSYSTEMD  || { echo -e $RED'Modifying GROUP in SYSTEMD file failed.'$ENDCOLOR; exit 1; }

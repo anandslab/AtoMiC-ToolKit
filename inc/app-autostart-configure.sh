02cc8c5a9f3a05729d897040e9c51f699e036a48
TommyE123 <Tom.Eley@gmail.com>
2017-01-15 16:27:24 +0000

    Further autostart changes

--- inc/app-autostart-configure.sh
+++ inc/app-autostart-configure.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 if [[ `systemctl` =~ -\.mount ]]; then 
+    STARTUPTYPE='systemd'
     echo using systemd
     source $SCRIPTPATH/inc/app-systemd-add.sh
     source $SCRIPTPATH/$APPNAME/$APPNAME-systemd-update.sh
 elif [[ -f /etc/init.d/cron && ! -h /etc/init.d/cron ]]; then 
+    STARTUPTYPE='sysv-init'
     echo using sysv-init
+    source $SCRIPTPATH/inc/app-init-add.sh
 else 
+    STARTUPTYPE='unknown'
     echo 'cannot tell Startup type'
 fi

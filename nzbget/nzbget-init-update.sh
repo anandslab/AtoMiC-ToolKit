ac93dee7b163e7842b60f3899a462fbd2be6f2da
TommyE123 <Tom.Eley@gmail.com>
2017-01-15 22:24:27 +0000

    NZBGet startup fix

--- nzbget/nzbget-init-update.sh
+++ nzbget/nzbget-init-update.sh
@@ -12,3 +12,4 @@ sudo sed -i "s@NZBGET_GROUP=nzb@NZBGET_GROUP=$UGROUP@g" /etc/init.d/$APPNAME || 
 sudo sed -i "s@NZBGET_CONF=/etc/nzbget.conf@NZBGET_CONF=$APPSETTINGS@g" /etc/init.d/$APPNAME || { echo -e $RED'Replacing NZBGET_CONF path failed.'$ENDCOLOR ; exit 1; }
 sudo sed -i "s@NZBGET_BIN=/usr/local/bin/nzbget@NZBGET_BIN=$NZBGET_BIN@g" /etc/init.d/$APPNAME || { echo -e $RED'Replacing NZBGET_BIN failed.'$ENDCOLOR ; exit 1; }
 sudo sed -i "s@PID_FILE=/home/user/downloads/nzbget.lock@PID_FILE=$PID_FILE@g" /etc/init.d/$APPNAME || { echo -e $RED'Replacing PID_FILE failed.'$ENDCOLOR ; exit 1; }
+sudo update-rc.d $APPNAME defaults

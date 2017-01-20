bd7e4f8c98fb82b56399ec01570ec4af07c9bbf1
TommyE123 <Tom.Eley@gmail.com>
2017-01-20 20:47:57 +0000

    Fixed access switch

--- inc/app-access-switch.sh
+++ inc/app-access-switch.sh
@@ -7,13 +7,13 @@
 
 # DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
 
-if [[ $ISSETUP != "Yes" ]]
-then
+if [[ $ISSETUP != "Yes" ]]; then
   echo
   echo -e '\e[91mCannot be run directly. Please run setup.sh from AtoMiC ToolKit root folder: \033[0msudo bash setup.sh'
   echo
   exit 0
 fi
+
 source $SCRIPTPATH/inc/commons.sh
 source $SCRIPTPATH/inc/header.sh
 
@@ -23,27 +23,25 @@ source $SCRIPTPATH/inc/pause.sh
 source $SCRIPTPATH/inc/app-folder-check.sh
 source $SCRIPTPATH/inc/app-settings-check.sh
 
-  echo 
-  sleep 2
-  source $SCRIPTPATH/inc/app-stop.sh
-  sleep 2
-  echo
+echo 
+sleep 2
+source $SCRIPTPATH/inc/app-stop.sh
+sleep 2
+echo
 
-  if grep -Exq "${ACCESSPORT}localhost" $APPSETTINGS
-  then
-    echo -e $YELLOW'--->Making '$APPTITLE' accessible from network...'$ENDCOLOR
-    sudo sed -i "s@${ACCESSPORT}localhost@${ACCESSPORT}0.0.0.0@g" $APPSETTINGS  || { echo -e $RED'Modifying '$ACCESSPORT' in '$APPSETTINGS' file failed.'$ENDCOLOR; exit 1; }
-  else
-    echo -e $YELLOW'--->Making '$APPTITLE' only accessible from localhost...'$ENDCOLOR
-    sudo sed -i "s@${ACCESSPORT}0.0.0.0@${ACCESSPORT}localhost@g" $APPSETTINGS  || { echo -e $RED'Modifying '$ACCESSPORT' in '$APPSETTINGS' file failed.'$ENDCOLOR; exit 1; }
-  fi
-
-  sleep 2
-  source $SCRIPTPATH/inc/app-start.sh
-  sleep 2
-  echo
-  echo -e $GREEN'--->All done. '$APPTITLE' access switch complete.'$ENDCOLOR
+if grep -Exq "${ACCESSPORT}localhost" $APPSETTINGS; then
+  echo -e $YELLOW'--->Making '$APPTITLE' accessible from network...'$ENDCOLOR
+  sudo sed -i "s@${ACCESSPORT}localhost@${ACCESSPORT}0.0.0.0@g" $APPSETTINGS  || { echo -e $RED'Modifying '$ACCESSPORT' in '$APPSETTINGS' file failed.'$ENDCOLOR; exit 1; }
+else
+  echo -e $YELLOW'--->Making '$APPTITLE' only accessible from localhost...'$ENDCOLOR
+  sudo sed -i "s@${ACCESSPORT}0.0.0.0@${ACCESSPORT}localhost@g" $APPSETTINGS  || { echo -e $RED'Modifying '$ACCESSPORT' in '$APPSETTINGS' file failed.'$ENDCOLOR; exit 1; }
 fi
+
+sleep 2
+source $SCRIPTPATH/inc/app-start.sh
+sleep 2
+echo
+echo -e $GREEN'--->All done. '$APPTITLE' access switch complete.'$ENDCOLOR
 
 source $SCRIPTPATH/inc/thankyou.sh
 source $SCRIPTPATH/inc/exit.sh

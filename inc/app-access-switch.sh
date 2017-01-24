fb4679676f910174b15f20af61aec2af3ad2e174
TommyE123 <Tom.Eley@gmail.com>
2017-01-24 17:49:17 +0000

    Access Switch now works with more apps

--- inc/app-access-switch.sh
+++ inc/app-access-switch.sh
@@ -29,12 +29,12 @@ source $SCRIPTPATH/inc/app-stop.sh
 sleep 2
 echo
 
-if grep -Exq "${ACCESSHOST}localhost" $APPSETTINGS; then
-  echo -e $YELLOW'--->Making '$APPTITLE' accessible from network...'$ENDCOLOR
-  sudo sed -i "s@${ACCESSHOST}localhost@${ACCESSHOST}0.0.0.0@g" $APPSETTINGS  || { echo -e $RED'Modifying '$ACCESSHOST' in '$APPSETTINGS' file failed.'$ENDCOLOR; exit 1; }
-else
+if grep -Exq "${ACCESSHOST}0.0.0.0" $APPSETTINGS; then
   echo -e $YELLOW'--->Making '$APPTITLE' only accessible from localhost...'$ENDCOLOR
-  sudo sed -i "s@${ACCESSHOST}0.0.0.0@${ACCESSHOST}localhost@g" $APPSETTINGS  || { echo -e $RED'Modifying '$ACCESSHOST' in '$APPSETTINGS' file failed.'$ENDCOLOR; exit 1; }
+  sudo sed -i "s@^${ACCESSHOST}.*@${ACCESSHOST}localhost@g" $APPSETTINGS  || { echo -e $RED'Modifying '$ACCESSHOST' in '$APPSETTINGS' file failed.'$ENDCOLOR; exit 1; }
+else
+  echo -e $YELLOW'--->Making '$APPTITLE' accessible from network...'$ENDCOLOR
+  sudo sed -i "s@^${ACCESSHOST}localhost@${ACCESSHOST}0.0.0.0@g" $APPSETTINGS  || { echo -e $RED'Modifying '$ACCESSHOST' in '$APPSETTINGS' file failed.'$ENDCOLOR; exit 1; }
 fi
 
 sleep 2

a22f0a3518367a4e31e245fa5e3c6b198ad199a5
TommyE123 <Tom.Eley@gmail.com>
2017-01-31 19:50:37 +0000

    SickGear - Fixed missing password reset

--- sickgear/sickgear-default-update.sh
+++ sickgear/sickgear-default-update.sh
@@ -6,11 +6,11 @@
 
 # DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
 
-#$RUN_AS, username to run sickrage under, the default is sickrage
+#$RUN_AS, username to run sickgear under, the default is sickgear
 sudo sed -i "s@SG_USER=@SG_USER=$UNAME@g" /etc/default/$APPNAME || { echo -e $RED'Replacing RUN_AS failed.'$ENDCOLOR ; exit 1; }
 
-#$APP_PATH, the location of SickBeard.py, the default is /opt/sickrage
+#$APP_PATH, the location of SickBeard.py, the default is /opt/sickgear
 sudo sed -i "s@SG_HOME=@SG_HOME=$APPPATH@g" /etc/default/$APPNAME || { echo -e $RED'Replacing RUN_AS failed.'$ENDCOLOR ; exit 1; }
 
-#$DATA_DIR, the location of sickbeard.db, cache, logs, the default is /opt/sickrage
+#$DATA_DIR, the location of sickbeard.db, cache, logs, the default is /opt/sickgear
 sudo sed -i "s@SG_DATA=@SG_DATA=$APPPATH/data@g" /etc/default/$APPNAME || { echo -e $RED'Replacing DATADIR failed.'$ENDCOLOR ; exit 1; }

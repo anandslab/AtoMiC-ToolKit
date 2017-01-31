c4663fb990cae3014007d9e79647ef6a32bada69
“AtoMiCUser” <atomic@htpcbeginner.com>
2017-01-31 21:11:32 +0000

    SickRage - Added restart on Failure

--- sickrage/sickrage-systemd-update.sh
+++ sickrage/sickrage-systemd-update.sh
@@ -9,7 +9,7 @@
 sudo sed -i "s@Description=SickRage Daemon@Description=SickRage@g" /etc/systemd/system/$APPSYSTEMD  || { echo -e $RED'Modifying DESCRIPTION in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
 sudo sed -i "s@User=sickrage@User=$UNAME@g" /etc/systemd/system/$APPSYSTEMD  || { echo -e $RED'Modifying USER in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
 sudo sed -i "s@Group=sickrage@Group=$UGROUP@g" /etc/systemd/system/$APPSYSTEMD  || { echo -e $RED'Modifying GROUP in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
-
+sudo sed -i '/^GuessMainPID=no/a Restart=on-failure' /etc/systemd/system/$APPSYSTEMD  || { echo -e $RED'Adding RESTART in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
 sudo sed -i "s@ExecStart=/usr/bin/python2.7 /opt/sickrage/SickBeard.py -q --daemon --nolaunch --datadir=/opt/sickrage@ExecStart=/usr/bin/python $APPPATH/SickBeard.py -q --daemon --nolaunch --datadir=$APPPATH/data@g" /etc/systemd/system/$APPSYSTEMD  || { echo -e $RED'Modifying ExecStart in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
 
 sudo systemctl daemon-reload

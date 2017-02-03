a8bcefd14742be8bf58c1c03e0b0684e2c941446
TommyE123 <Tom.Eley@gmail.com>
2017-02-03 12:35:54 +0000

    Webmin - Uses generic backup

--- webmin/webmin-menu.sh
+++ webmin/webmin-menu.sh
@@ -21,7 +21,7 @@ if [ $exitstatus = 0 ]; then
     case "$SUBCHOICE" in 
 		"Install" ) source $SCRIPTPATH/webmin/webmin-installer.sh ;;
 		"Uninstall" ) source $SCRIPTPATH/webmin/webmin-uninstaller.sh ;;
-		"Backup" ) source $SCRIPTPATH/webmin/webmin-backup.sh ;;
+		"Backup" ) source $SCRIPTPATH/inc/app-backup-controller.sh ;;
 		"Restore" ) source $SCRIPTPATH/webmin/webmin-restore.sh ;;
 		"Manual Update" ) source $SCRIPTPATH/webmin/webmin-update.sh ;;
         "Access Details" ) source $SCRIPTPATH/inc/app-access-details.sh ;;

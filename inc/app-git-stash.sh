978445cb296ae04b2fc1672c50c46ff1351b5dbb
TommyE123 <Tom.Eley@gmail.com>
2017-02-03 12:33:20 +0000

    Removed Git Stash username
    This was causing branches to revert to a broken username\email.

--- inc/app-git-stash.sh
+++ inc/app-git-stash.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo
 sleep 1
-echo -e $YELLOW'--->Git stashing any local changes made to '$APPTITLE'...'$ENDCOLOR
-cd $APPPATH
-git config user.email “atomic@htpcbeginner.com”
-git config user.name “AtoMiCUser”
-git stash
-git stash clear
+echo -e $YELLOW"--->Git stashing any local changes made to $APPTITLE..."$ENDCOLOR
+git -C $APPPATH stash
+git  -C $APPPATH stash clear

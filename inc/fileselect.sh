a1848710e17a76aa78810be0027be88179ec3b01
TommyE123 <Tom.Eley@gmail.com>
2017-02-03 13:25:01 +0000

    Fixed broken menu call

--- inc/fileselect.sh
+++ inc/fileselect.sh
@@ -14,7 +14,7 @@ done
 if [[ -z "$FILELIST" ]]; then
 	echo -e $RED'No '$APPNAME' backups were found in '$CYAN$SCRIPTPATH'/backups folder. Cannot perform restore. Returning to menu.'$ENDCOLOR
     source $SCRIPTPATH/inc/pause.sh
-    source $SCRIPTPATH/$APPNAME/menu-$APPNAME.sh
+    source $SCRIPTPATH/$APPNAME/$APPNAME-menu.sh
 else 
 	FILECHOICE=$(whiptail --title "AtoMiC ToolKit - Restore Backup" --menu "Select the backup file you want to restore:" --backtitle "$BACKTITLE" --fb --ok-button "Select" $LINES $COLUMNS $NETLINES "${FILELIST[@]}" 3>&1 1>&2 2>&3)
 	exitstatus=$?

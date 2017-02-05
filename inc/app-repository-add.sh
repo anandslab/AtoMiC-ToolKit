6419b0b8e02a7da9f4848ff451faa2d86ca8e40b
TommyE123 <Tom.Eley@gmail.com>
2017-02-05 14:40:11 +0000

    Don't change repo if we've not set one

--- inc/app-repository-add.sh
+++ inc/app-repository-add.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 echo
-echo -e $YELLOW"--->Adding $APPTITLE repository..."$ENDCOLOR
-GREPOUT=$(grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep $APPREPOSITORYNAME)
-if [ "$GREPOUT" == "" ]; then
-    if [ "$REPOPPA" = 'YES' ]; then
-        sudo add-apt-repository -y $APPREPOSITORYLINK
+if [ ! "$APPREPOSITORYLINK" == "" ]; then
+
+    echo -e $YELLOW"--->Adding $APPTITLE repository..."$ENDCOLOR
+    GREPOUT=$(grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep $APPREPOSITORYNAME)
+    if [ "$GREPOUT" == "" ]; then
+        if [ "$REPOPPA" = 'YES' ]; then
+            sudo add-apt-repository -y $APPREPOSITORYLINK
+        else
+            echo "$APPREPOSITORYLINK" | sudo tee -a /etc/apt/sources.list.d/$APPNAME.list
+
+            if [[ $APPREPOSITORYLINKBACKUP != "" ]]; then
+                echo "$APPREPOSITORYLINKBACKUP" | sudo tee -a /etc/apt/sources.list.d/$APPNAME.list
+            fi
+
+            if [ "$REPOKEYSREQ" = 'YES' ]; then
+                source $SCRIPTPATH/inc/app-keys-add.sh
+            fi
+        fi
     else
-        echo "$APPREPOSITORYLINK" | sudo tee -a /etc/apt/sources.list.d/$APPNAME.list
-
-        if [[ $APPREPOSITORYLINKBACKUP != "" ]]; then
-            echo "$APPREPOSITORYLINKBACKUP" | sudo tee -a /etc/apt/sources.list.d/$APPNAME.list
-        fi
-
-        if [ "$REPOKEYSREQ" = 'YES' ]; then
-            source $SCRIPTPATH/inc/app-keys-add.sh
-        fi
+        echo "$APPTITLE repository already exists"
     fi
-else
-    echo "$APPTITLE repository already exists"
 fi
--- inc/app-repository-remove.sh
+++ inc/app-repository-remove.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 echo
-echo -e $YELLOW"--->Removing $APPTITLE repository..."$ENDCOLOR
-GREPOUT=$(grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep $APPREPOSITORYNAME)
-if ! [ "$GREPOUT" == "" ]; then
-    if [ "$REPOPPA" = 'YES' ]; then
-        sudo add-apt-repository -r -y $APPREPOSITORYLINK
+if [ ! "$APPREPOSITORYLINK" == "" ]; then
+    echo -e $YELLOW"--->Removing $APPTITLE repository..."$ENDCOLOR
+    GREPOUT=$(grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep $APPREPOSITORYNAME)
+    if ! [ "$GREPOUT" == "" ]; then
+        if [ "$REPOPPA" = 'YES' ]; then
+            sudo add-apt-repository -r -y $APPREPOSITORYLINK
+        else
+            sudo rm /etc/apt/sources.list.d/$APPNAME.list
+            echo 'OK'
+        fi
+
+        source $SCRIPTPATH/inc/app-keys-remove.sh
+
     else
-        sudo rm /etc/apt/sources.list.d/$APPNAME.list
-        echo 'OK'
+        echo $APPTITLE' repository doesnt exist'
     fi
-
-    source $SCRIPTPATH/inc/app-keys-remove.sh
-
-else
-    echo $APPTITLE' repository doesnt exist'
 fi

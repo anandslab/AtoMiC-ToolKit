4db8cbfd81d7d395f8bf2705fe80b5a60979b3c4
TommyE123 <Tom.Eley@gmail.com>
2017-02-05 18:34:35 +0000

    Added Mono installer

---
+++ utils/mono/mono-installer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script Name: AtoMiC Mono Installer
+# Author: htpcBeginner
+# Publisher: http://www.htpcBeginner.com
+# License: MIT License (refer to README.md for more details)
+#
+
+# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+echo -e $GREEN"AtoMiC Mono Installer Script"$ENDCOLOR
+source $SCRIPTPATH/utils/mono/mono-constants.sh
+source $SCRIPTPATH/utils/mono/mono-repository-configurator.sh
+source $SCRIPTPATH/inc/app-repository-add.sh
+source $SCRIPTPATH/inc/pkgupdate.sh
+source $SCRIPTPATH/inc/app-install.sh
+source $SCRIPTPATH/inc/app-install-deps.sh
+source $SCRIPTPATH/utils/mono/mono-constants-reset.sh
+echo -e $GREEN"---> Mono installation complete."$ENDCOLOR

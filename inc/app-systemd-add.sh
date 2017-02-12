26fac9495f2a70e1b7975041f0f088c5f2bd69b7
TommyE123 <Tom.Eley@gmail.com>
2017-02-12 19:27:18 +0000

    Minor updates

--- inc/app-systemd-add.sh
+++ inc/app-systemd-add.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo
 sleep 1
-echo -e $YELLOW'--->Enabling '$APPTITLE' Autostart at Boot...'$ENDCOLOR
+echo -e $YELLOW"--->Enabling $APPTITLE Autostart at Boot..."$ENDCOLOR
 sudo cp $APPSYSTEMDLOC /etc/systemd/system/$APPSYSTEMD || { echo $RED'Creating systemd file failed.'$ENDCOLOR ; exit 1; }
 sudo chmod 644 /etc/systemd/system/$APPSYSTEMD
 sudo systemctl enable $APPSYSTEMD
-echo "/etc/systemd/system/$APPSYSTEMD SystemD script added and enabled"
+echo "$APPSYSTEMD SystemD script added and enabled"

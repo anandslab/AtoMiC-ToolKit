fa5790606fdc1a499bcd2b3e3bbce6029a002495
TommyE123 <Tom.Eley@gmail.com>
2017-02-12 19:26:05 +0000

    Added FFmpeg installer

--- utils/ffmpeg/ffmpeg-installer.sh
+++ utils/ffmpeg/ffmpeg-installer.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 # Script Name: AtoMiC FFmpeg Installer
-# Author: htpcBeginner
+# Author: TommyE123
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)
 #
 
 # DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
-
+echo
 echo -e $GREEN"AtoMiC FFmpeg Installer Script"$ENDCOLOR
 
+ARCHDETECT=$(uname -m)
 
+if [ "$ARCHDETECT" = 'armv?l' ]; then
+    ARCH='release-armhf'
+elif [ "$ARCHDETECT" = 'armel' ]; then
+    ARCH='release-armel'
+elif [ "$ARCHDETECT" = 'i?86' ]; then
+    ARCH='git-32bit'
+elif [ "$ARCHDETECT" = 'x86_64' ]; then
+    ARCH='git-64bit'
+fi
 
-echo -e $GREEN"---> FFmpeg installation complete."$ENDCOLOR
+if ! [ "$ARCH" = '' ]; then
+    echo -e $YELLOW'--->Downloading and extracting files...'$ENDCOLOR
+    URL=https://johnvansickle.com/ffmpeg/builds/ffmpeg-$ARCH-static.tar.xz
+    echo $URL
+    TMP_DIR=$( mktemp -d )
+    curl --silent $URL | sudo tar Jx -C $TMP_DIR --strip-components=1
+    mv $TMP_DIR/ff* /usr/bin
+    rm -rf $TMP_DIR
+    echo -e $GREEN"---> FFmpeg installation complete."$ENDCOLOR
+else
+    echo -e $RED'--->Unable to detect architecture to install FFmpeg...'$ENDCOLOR
+fi
---
+++ utils/ffmpeg/ffmpeg-menu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [[ $ISSETUP != "Yes" ]]
+then
+  echo
+  echo -e '\e[91mCannot be run directly. Please run setup.sh from AtoMiC ToolKit root folder: \033[0msudo bash setup.sh'
+  echo
+  exit 0
+fi
+SUBCHOICE=$(whiptail --title "AtoMiC ToolKit - Manage FFmpeg" --menu "What would you like to do?" --backtitle "$BACKTITLE" --fb --cancel-button "Exit" $LINES $COLUMNS $NETLINES \
+"Install\Update" "FFmpeg, FFmpeg-10bit, FFprobe, FFserver" \
+"Uninstall" "FFmpeg, FFmpeg-10bit, FFprobe, FFserver" \
+"Go Back" "Back to Main Menu" 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    case "$SUBCHOICE" in 
+		"Install\Update" ) source $SCRIPTPATH/utils/ffmpeg/ffmpeg-installer.sh ;;
+		"Uninstall" ) source $SCRIPTPATH/utils/ffmpeg/ffmpeg-uninstaller.sh ;;
+		"Go Back" ) source $SCRIPTPATH/utils/utils-menu.sh ;;
+		*) source $SCRIPTPATH/inc/invalid-option.sh ;;
+	esac
+else
+    source $SCRIPTPATH/inc/thankyou.sh
+    echo
+    sleep 1
+    exit 1
+fi

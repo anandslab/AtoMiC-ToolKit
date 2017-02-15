269ec149509ce833addef738c4fa60305d1e42f1
TommyE123 <Tom.Eley@gmail.com>
2017-02-15 16:56:22 +0000

    Repository name fixes
    This fixes the potential to add multiple repositories of the same name. This was causing errors while updating. Also changed the following repo names to reflect what the official instructions would use:
    emby
    sonarr
    mono

--- inc/app-repository-add.sh
+++ inc/app-repository-add.sh
@@ -3,15 +3,16 @@ echo
 if [ ! "$APPREPOSITORYLINK" == "" ]; then
 
     echo -e $YELLOW"--->Adding $APPTITLE repository..."$ENDCOLOR
-    GREPOUT=$(grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep $APPREPOSITORYNAME)
-    if [ "$GREPOUT" == "" ]; then
+    if ! grep -rq "$APPREPOSITORYLINK" /etc/apt; then
         if [ "$REPOPPA" = 'YES' ]; then
             sudo add-apt-repository -y $APPREPOSITORYLINK
         else
-            echo "$APPREPOSITORYLINK" | sudo tee -a /etc/apt/sources.list.d/$APPNAME.list
-
-            if [[ $APPREPOSITORYLINKBACKUP != "" ]]; then
-                echo "$APPREPOSITORYLINKBACKUP" | sudo tee -a /etc/apt/sources.list.d/$APPNAME.list
+            echo "$APPREPOSITORYLINK" | sudo tee -a /etc/apt/sources.list.d/$APPREPOSITORYNAME.list
+            
+            if ! grep -rq "$APPREPOSITORYLINK" /etc/apt; then
+                if [[ $APPREPOSITORYLINKBACKUP != "" ]]; then
+                    echo "$APPREPOSITORYLINKBACKUP" | sudo tee -a /etc/apt/sources.list.d/$APPREPOSITORYNAME.list
+                fi
             fi
 
             if [ "$REPOKEYSREQ" = 'YES' ]; then

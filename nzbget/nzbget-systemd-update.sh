a230d97728145487930980f5b1e6a3e4137f73cd
Tom <TommyE123@users.noreply.github.com>
2017-02-19 09:21:31 +0000

    Reversed changes to add missing lines

--- nzbget/nzbget-systemd-update.sh
+++ nzbget/nzbget-systemd-update.sh
@@ -10,5 +10,9 @@
 sudo sed -i "s@User=ReplaceMe@User=$UNAME@g" /etc/systemd/system/$APPSYSTEMD  || { echo -e $RED'Modifying USER in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
 sudo sed -i "s@Group=ReplaceMe@Group=$UGROUP@g" /etc/systemd/system/$APPSYSTEMD  || { echo -e $RED'Modifying GROUP in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
 
+sudo sed -i "s@ExecStart=</path/to/nzbget/nzbget> -D@ExecStart=$APPPATH/nzbget -D@g" /etc/systemd/system/$APPSYSTEMD  || { echo -e $RED'Modifying GROUP in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
+sudo sed -i "s@ExecStop=</path/to/nzbget/nzbget> -Q@ExecStop=$APPPATH/nzbget -Q@g" /etc/systemd/system/$APPSYSTEMD  || { echo -e $RED'Modifying GROUP in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
+sudo sed -i "s@ExecReload=</path/to/nzbget/nzbget> -O@ExecReload=$APPPATH/nzbget -O@g" /etc/systemd/system/$APPSYSTEMD  || { echo -e $RED'Modifying GROUP in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
+  
 sudo systemctl daemon-reload
 sudo systemctl enable $APPSYSTEMD

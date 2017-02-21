80f0569b2f25909a7abfd044210d9ccda6c0a586
TommyE123 <Tom.Eley@gmail.com>
2017-02-21 07:31:27 +0000

    Emby - Script name change

--- emby-server/emby-server-uninstaller.sh
+++ emby-server/emby-server-uninstaller.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script Name: AtoMiC SABnzbd Uninstaller
+# Script Name: AtoMiC Emby Uninstaller
 # Author: TommyE123
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)

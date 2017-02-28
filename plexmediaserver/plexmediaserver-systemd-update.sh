4f130148f87af26e6ee786850b293f197e7820bd
TommyE123 <Tom.Eley@gmail.com>
2017-02-28 21:13:40 +0000

    Plex - enable the added systemd

--- plexmediaserver/plexmediaserver-systemd-update.sh
+++ plexmediaserver/plexmediaserver-systemd-update.sh
@@ -11,4 +11,9 @@ echo -e $YELLOW"--->Updating systemd to run as $CYAN$UNAME$ENDCOLOR ..."$ENDCOLO
 
 sudo sed -i "s@User=ReplaceMe@User=$UNAME@g" /etc/systemd/system/plexmediaserver.service.d/override.conf || { echo -e $RED'Modifying USER in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
 sudo sed -i "s@Group=ReplaceMe@Group=$UGROUP@g" /etc/systemd/system/plexmediaserver.service.d/override.conf || { echo -e $RED'Modifying GROUP in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
+
+#Probably not required most of the time unless we've actually added a systemd file'
+sudo systemctl daemon-reload >/dev/null 2>&1
+sudo systemctl enable $APPSYSTEMD  >/dev/null 2>&1
+
 echo 'OK'

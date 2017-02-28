faf649f2f03783967bc45045a596b1810814ba5e
TommyE123 <Tom.Eley@gmail.com>
2017-02-28 17:04:53 +0000

    Kodi Added

--- kodi/kodi-constants.sh
+++ kodi/kodi-constants.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 APPNAME='kodi'
 APPSHORTNAME='kodi'
-APPPATH=''
 APPTITLE='Kodi'
-APPDEPS='sndiod kodi-pvr-mythtv kodi-pvr-vuplus kodi-pvr-vdr-vnsi kodi-pvr-njoy kodi-pvr-nextpvr kodi-pvr-mediaportal-tvserver kodi-pvr-tvheadend-hts kodi-pvr-dvbviewer kodi-pvr-argustv kodi-pvr-iptvsimple kodi-audioencoder-vorbis kodi-audioencoder-flac kodi-audioencoder-lame'
-APPDPORT=''
-APPSETTINGS=''
-APPREPOSITORYNAME=''
+APPDEPS='xorg dbus-x11 sndiod kodi-pvr-mythtv kodi-pvr-vuplus kodi-pvr-vdr-vnsi kodi-pvr-njoy kodi-pvr-nextpvr kodi-pvr-mediaportal-tvserver kodi-pvr-tvheadend-hts kodi-pvr-dvbviewer kodi-pvr-argustv kodi-pvr-iptvsimple kodi-audioencoder-vorbis kodi-audioencoder-flac kodi-audioencoder-lame'
+APPSYSTEMD='kodi.service'
+APPSYSTEMDLOC=$SCRIPTPATH/$APPNAME/$APPSYSTEMD
+APPINITDLOC=$SCRIPTPATH/$APPNAME'/kodi.init'
--- kodi/kodi-menu.sh
+++ kodi/kodi-menu.sh
@@ -8,6 +8,8 @@ then
 fi
 SUBCHOICE=$(whiptail --title "AtoMiC ToolKit - Manage Kodi" --menu "What would you like to do?" --backtitle "$BACKTITLE" --fb --cancel-button "Exit" $LINES $COLUMNS $NETLINES \
 "Install" "Install Kodi" \
+"Enable" "Kodi to Launch on Boot" \
+"Disable" "Kodi to Launch on Boot" \
 "Uninstall" "Uninstall Kodi" \
 "Manual Update" "Manually update Kodi" \
 "Go Back" "Back to Main Menu" 3>&1 1>&2 2>&3)
@@ -17,6 +19,12 @@ if [ $exitstatus = 0 ]; then
 	source $SCRIPTPATH/kodi/kodi-constants.sh
     case "$SUBCHOICE" in 
 		"Install" ) source $SCRIPTPATH/kodi/kodi-installer.sh ;;
+    "Enable" ) source $SCRIPTPATH/inc/app-autostart-configure.sh 
+               source $SCRIPTPATH/inc/pause.sh
+               source $SCRIPTPATH/kodi/kodi-menu.sh;;
+    "Disable" ) source $SCRIPTPATH/inc/app-autostart-remove.sh 
+                source $SCRIPTPATH/inc/pause.sh
+                source $SCRIPTPATH/kodi/kodi-menu.sh;;
 		"Uninstall" ) source $SCRIPTPATH/kodi/kodi-uninstaller.sh ;;
 		"Manual Update" ) source $SCRIPTPATH/kodi/kodi-update.sh ;;
 		"Go Back" ) source $SCRIPTPATH/menus/menu-personal-media-servers.sh ;;

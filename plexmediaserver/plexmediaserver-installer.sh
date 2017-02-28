64089d6f6462238c7b33ed48a78edfb048017df8
TommyE123 <Tom.Eley@gmail.com>
2017-02-28 19:43:37 +0000

    Plex - Added fixes for ARM devices

--- plexmediaserver/plexmediaserver-installer.sh
+++ plexmediaserver/plexmediaserver-installer.sh
@@ -20,16 +20,19 @@ source $SCRIPTPATH/inc/header.sh
 echo -e $GREEN"AtoMiC $APPTITLE Installer Script"$ENDCOLOR
 echo
 
+source $SCRIPTPATH/inc/app-stop.sh
 source $SCRIPTPATH/$APPNAME/$APPNAME-repository-configurator.sh
 source $SCRIPTPATH/inc/app-repository-add.sh
 source $SCRIPTPATH/inc/pkgupdate.sh
 source $SCRIPTPATH/utils/ffmpeg/ffmpeg-installer.sh
 source $SCRIPTPATH/plexmediaserver/plexmediaserver-constants.sh
 source $SCRIPTPATH/inc/app-folders-create.sh
+#Copies over a service file that will get overridden unless one isnt created. (Dev2day repo currently doesnt)
 source $SCRIPTPATH/inc/app-autostart-configure.sh
 source $SCRIPTPATH/inc/app-install.sh
+source $SCRIPTPATH/inc/app-autostart-remove-unrequired-only.sh
 source $SCRIPTPATH/inc/app-install-confirmation.sh
-echo 'Plex should start within a few seconds'
+echo 'Plex should start within a few seconds depending on the speed of your device.'
 echo -e 'Open '$CYAN'http://localhost:32400/web'$ENDCOLOR' in your browser to continue the setup.'
 source $SCRIPTPATH/inc/thankyou.sh
 source $SCRIPTPATH/inc/exit.sh

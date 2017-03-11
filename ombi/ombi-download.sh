ee16c3d081d02c0f7a92e422723cc6a0b0723201
Mathew Giljum <mathew.giljum@gmail.com>
2017-03-11 16:17:35 -0500

    Add sudo to unzip, make quiet

--- ombi/ombi-download.sh
+++ ombi/ombi-download.sh
@@ -15,5 +15,5 @@ echo -e $YELLOW'--->Downloading files...'$ENDCOLOR
 sudo rm -rf /tmp/ombidownload
 sudo wget -P /tmp/ombidownload "${OUTPUT}"
 echo -e $YELLOW'--->Extracting files...'$ENDCOLOR
-unzip /tmp/ombidownload/*.zip -d /tmp/ombidownload/extract
+sudo unzip -q /tmp/ombidownload/*.zip -d /tmp/ombidownload/extract
 sudo mv /tmp/ombidownload/extract/Release/* $APPPATH

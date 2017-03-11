8dbe949731b4d422600ef1776aa0a772fa3882ca
Mathew Giljum <mathew.giljum@gmail.com>
2017-03-11 15:49:36 -0500

    Fix download for zip release
    Ombi's compiled release only comes in .zip, so curl piped to tar fails.
    For now, the download script will use wget/unzip using a temporary
    directory

--- ombi/ombi-download.sh
+++ ombi/ombi-download.sh
@@ -11,5 +11,9 @@ echo -e $YELLOW'--->Latest File Found...'$ENDCOLOR
 OUTPUT="$(curl -s https://api.github.com/repos/tidusjar/Ombi/releases| grep browser_download_url | head -n 1 | cut -d '"' -f 4)"
 echo "${OUTPUT}"
 echo
-echo -e $YELLOW'--->Downloading and extracting files...'$ENDCOLOR
-sudo curl -L "${OUTPUT}" | unzip - -d $APPPATH
+echo -e $YELLOW'--->Downloading files...'$ENDCOLOR
+sudo rm -rf /tmp/ombidownload
+sudo wget -P /tmp/ombidownload "${OUTPUT}"
+echo -e $YELLOW'--->Extracting files...'$ENDCOLOR
+unzip -j /tmp/ombidownload/*.zip -d /tmp/ombidownload/extract
+sudo mv /tmp/ombidownload/extract/Release/* $APPPATH

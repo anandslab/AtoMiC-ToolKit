d0588ff81acf2beee803db7ac2aa916461a11d7b
TommyE123 <Tom.Eley@gmail.com>
2017-03-13 19:11:44 +0000

    added Madsonic

---
+++ madsonic/madsonic-ffmpeg-update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script Name: AtoMiC Madsonic ffmpeg update
+# Author: TommyE123
+# Publisher: http://www.htpcBeginner.com
+# License: MIT License (refer to README.md for more details)
+
+# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+source $SCRIPTPATH/utils/ffmpeg/ffmpeg-installer.sh
+echo
+echo -e $YELLOW'--->Copying ffmpeg file to /var/madsonic/transcode/...'$ENDCOLOR
+yes | cp -rf /usr/bin/ffmpeg /var/madsonic/transcode/
+source $SCRIPTPATH/madsonic/madsonic-constants.sh
---
+++ madsonic/madsonic-menu.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+if [[ $ISSETUP != "Yes" ]]
+then
+  echo
+  echo -e '\e[91mCannot be run directly. Please run setup.sh from AtoMiC ToolKit root folder: \033[0msudo bash setup.sh'
+  echo
+  exit 0
+fi
+SUBCHOICE=$(whiptail --title "AtoMiC ToolKit - Manage Madsonic" --menu "What would you like to do?" --backtitle "$BACKTITLE" --fb --cancel-button "Exit" $LINES $COLUMNS $NETLINES \
+"Install" "Install Madsonic" \
+"Uninstall" "Uninstall Madsonic" \
+"Backup" "Backup Madsonic settings" \
+"Restore" "Restore Madsonic settings from a previous backup" \
+"Manual Update" "Manually update Madsonic" \
+"Access Details" "View Madsonic access details" \
+"Go Back" "Back to Main Menu" 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+	source $SCRIPTPATH/madsonic/madsonic-constants.sh
+    case "$SUBCHOICE" in 
+		"Install" ) source $SCRIPTPATH/madsonic/madsonic-installer.sh ;;
+		"Uninstall" ) source $SCRIPTPATH/madsonic/madsonic-uninstaller.sh ;;
+		"Backup" ) source $SCRIPTPATH/inc/app-backup-controller.sh ;;
+		"Restore" ) source $SCRIPTPATH/inc/app-restore-controller.sh ;;
+		"Manual Update" ) source $SCRIPTPATH/madsonic/madsonic-update.sh ;;
+        "Access Details" ) source $SCRIPTPATH/inc/app-access-details.sh ;;
+		"Go Back" ) source $SCRIPTPATH/menus/menu-personal-media-servers.sh ;;
+		*) source $SCRIPTPATH/inc/invalid-option.sh ;;
+	esac
+else
+    source $SCRIPTPATH/inc/thankyou.sh
+    echo
+    sleep 1
+fi

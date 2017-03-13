468aa91dd7cb91d27cfde0d15aaa0cc0247252de
TommyE123 <Tom.Eley@gmail.com>
2017-03-13 21:28:55 +0000

    Added Watcher init script and other small tweaks

--- watcher/watcher-installer.sh
+++ watcher/watcher-installer.sh
@@ -3,7 +3,6 @@
 # Author: htpcBeginner
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)
-#
 
 # DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
 
--- watcher/watcher-menu.sh
+++ watcher/watcher-menu.sh
@@ -12,7 +12,6 @@ SUBCHOICE=$(whiptail --title "AtoMiC ToolKit - Manage Watcher" --menu "What woul
 "Backup" "Backup Watcher settings" \
 "Restore" "Restore Watcher settings from a previous backup" \
 "Manual Update" "Manually update Watcher" \
-"Reset Password" "Reset Watcher WebUI password" \
 "Access Details" "View Watcher access details" \
 "Go Back" "Back to Main Menu" 3>&1 1>&2 2>&3)
 
@@ -25,9 +24,8 @@ if [ $exitstatus = 0 ]; then
 		"Backup" ) source $SCRIPTPATH/inc/app-backup-controller.sh ;;
 		"Restore" ) source $SCRIPTPATH/inc/app-restore-controller.sh ;;
 		"Manual Update" ) source $SCRIPTPATH/watcher/watcher-update.sh ;;
-        "Reset Password" ) source $SCRIPTPATH/inc/app-password-reset-controller.sh ;;
         "Access Details" ) source $SCRIPTPATH/inc/app-access-details.sh ;;
-		"Go Back" ) source $SCRIPTPATH/menus/menu-usenet.sh ;;
+		"Go Back" ) source $SCRIPTPATH/menus/menu-pvrs.sh ;;
         *) source $SCRIPTPATH/inc/invalid-option.sh ;;
 	esac
 else

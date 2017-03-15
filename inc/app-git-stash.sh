8a81d7fe0f92c61aa47fd02b4880342dd7951724
Mathew Giljum <mathew.giljum@gmail.com>
2017-03-15 15:58:08 -0400

    set git user vars for stash
    fix issue #112

--- inc/app-git-stash.sh
+++ inc/app-git-stash.sh
@@ -2,5 +2,7 @@
 echo
 sleep 1
 echo -e $YELLOW"--->Git stashing any local changes made to $APPTITLE..."$ENDCOLOR
+git config user.name "atomic">/dev/null
+git config user.email "atomic@atomic.local">/dev/null
 git -C $APPPATH stash
 git  -C $APPPATH stash clear

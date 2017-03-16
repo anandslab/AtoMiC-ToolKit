ebc11210dbdb92c16db915723d9195368c412111
TommyE123 <Tom.Eley@gmail.com>
2017-03-16 19:42:10 +0000

    Jackett CI fixes

--- jackett/jackett-constants.sh
+++ jackett/jackett-constants.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2034
 APPNAME='jackett'
 APPSHORTNAME='jk'
 APPPATH='/opt/jackett'
--- jackett/jackett-update.sh
+++ jackett/jackett-update.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
+# shellcheck disable=SC1090
 # Script Name: AtoMiC Jackett Updater
 # Author: TommyE123
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)
-#
 
 # DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
 
-if [[ $ISSETUP != "Yes" ]]
-then
-  echo
-  echo -e '\e[91mCannot be run directly. Please run setup.sh from AtoMiC ToolKit root folder: \033[0msudo bash setup.sh'
-  echo
-  exit 0
+if [[ $ISSETUP != "Yes" ]]; then
+    echo
+    echo -e "${RED}Cannot be run directly. Please run setup.sh from \
+AtoMiC ToolKit root folder: ${ENDCOLOR}sudo bash setup.sh"
+    echo
+    exit 0
 fi
-source $SCRIPTPATH/inc/commons.sh
-source $SCRIPTPATH/inc/header.sh
+source "$SCRIPTPATH/inc/commons.sh"
+source "$SCRIPTPATH/inc/header.sh"
 
-echo -e $GREEN"AtoMiC $APPTITLE Update Script"$ENDCOLOR
+echo -e "${GREEN}AtoMiC $APPTITLE Update Script$ENDCOLOR"
 
-source $SCRIPTPATH/inc/pause.sh
-source $SCRIPTPATH/inc/app-folder-check.sh
-source $SCRIPTPATH/inc/app-stop.sh
-source $SCRIPTPATH/inc/pkgupdate.sh
-source $SCRIPTPATH/inc/app-install-deps.sh
-source $SCRIPTPATH/inc/app-folders-create.sh
-source $SCRIPTPATH/jackett/jackett-download.sh
-source $SCRIPTPATH/inc/app-set-permissions.sh
-source $SCRIPTPATH/inc/app-start.sh
-source $SCRIPTPATH/inc/app-update-confirmation.sh
-source $SCRIPTPATH/inc/thankyou.sh
-source $SCRIPTPATH/inc/exit.sh
+source "$SCRIPTPATH/inc/pause.sh"
+source "$SCRIPTPATH/inc/app-folder-check.sh"
+source "$SCRIPTPATH/inc/app-stop.sh"
+source "$SCRIPTPATH/inc/pkgupdate.sh"
+source "$SCRIPTPATH/inc/app-install-deps.sh"
+source "$SCRIPTPATH/inc/app-folders-create.sh"
+source "$SCRIPTPATH/jackett/jackett-download.sh"
+source "$SCRIPTPATH/inc/app-set-permissions.sh"
+source "$SCRIPTPATH/inc/app-start.sh"
+source "$SCRIPTPATH/inc/app-update-confirmation.sh"
+source "$SCRIPTPATH/inc/thankyou.sh"
+source "$SCRIPTPATH/inc/exit.sh"

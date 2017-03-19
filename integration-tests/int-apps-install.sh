a308be7c0663d303a97590d99d889d016b05f55c
TommyE123 <Tom.Eley@gmail.com>
2017-03-19 11:21:00 +0000

    Travis CI updated

--- integration-tests/int-apps-install.sh
+++ integration-tests/int-apps-install.sh
@@ -5,17 +5,10 @@
 # License: MIT License (refer to README.md for more details)
 
 # DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
-export CALLER=$(ps ax | grep "^ *$PPID" | awk '{print $NF}')
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-export SCRIPTPATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-mkdir "$SCRIPTPATH/tmp"
+mkdir "./tmp"
 
-echo "Agreed to disclaimers: `date '+%m-%d-%Y_%H-%M'`" >> "$SCRIPTPATH/tmp/consented"
-echo "UNAME=root" >> "$SCRIPTPATH/tmp/userinfo"
-echo "UGROUP=root" >> "$SCRIPTPATH/tmp/userinfo"
+echo "Agreed to disclaimers: `date '+%m-%d-%Y_%H-%M'`" >> "./tmp/consented"
+echo "UNAME=root" >> "./tmp/userinfo"
+echo "UGROUP=root" >> "./tmp/userinfo"
+
 sudo bash setup.sh -i webmin

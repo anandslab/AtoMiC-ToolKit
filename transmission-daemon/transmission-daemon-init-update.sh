5f6e13f054288266a85d3f38b82e6981b64bb0b8
TommyE123 <Tom.Eley@gmail.com>
2017-03-27 21:36:51 +0100

    Revert "Transmission - update upstart script"
    This reverts commit d6fdb8bac5a93d1ff40265c86250540c1b63efa1.

--- transmission-daemon/transmission-daemon-init-update.sh
+++ transmission-daemon/transmission-daemon-init-update.sh
@@ -7,11 +7,5 @@
 
 if [[ -f /etc/init.d/$APPNAME ]]; then
     echo 'Updating init.d file'
-    sudo sed -i "s@USER=debian-transmission@USER=$UNAME@g" /etc/init.d/transmission-daemon || { echo -e $RED'Replacing daemon username in init.d failed.'$ENDCOLOR ; exit 1; }
-fi
-
-if [[ -f /etc/init/transmission-daemon.conf ]]; then
-    echo 'Updating init file'
-    sudo sed -i "s@setuid\ debian-transmission@setuid\ $UNAME@g" /etc/init/transmission-daemon.conf || { echo -e $RED'Replacing daemon username in init failed.'$ENDCOLOR ; exit 1; }
-    sudo sed -i "s@setgid\ debian-transmission@setgid\ $UNAME@g" /etc/init/transmission-daemon.conf || { echo -e $RED'Replacing daemon group in init failed.'$ENDCOLOR ; exit 1; }
+    sudo sed -i "s@USER=debian-transmission@USER=$UNAME@g" /etc/init.d/transmission-daemon || { echo -e $RED'Replacing daemon username in init failed.'$ENDCOLOR ; exit 1; }
 fi

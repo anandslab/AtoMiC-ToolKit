6dc032590b8b17b491d128ba19b635440ae83c23
TommyE123 <Tom.Eley@gmail.com>
2017-03-28 20:45:05 +0100

    app install fixes

--- inc/app-autostart-remove.sh
+++ inc/app-autostart-remove.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# shellcheck disable=SC1090
 echo
-source $SCRIPTPATH/inc/app-init-remove.sh
-source $SCRIPTPATH/inc/app-systemd-remove.sh
+source "$SCRIPTPATH/inc/app-stop.sh"
+source "$SCRIPTPATH/inc/app-init-remove.sh"
+source "$SCRIPTPATH/inc/app-systemd-remove.sh"
 sleep 1
--- inc/app-install-pips.sh
+++ inc/app-install-pips.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo
 sleep 1
-echo -e $YELLOW'--->Installing pip prerequisites for '$APPTITLE'...'$ENDCOLOR
-pip install --user $APPPIPS
+echo -e "$YELLOW--->Installing pip prerequisites for $APPTITLE...$ENDCOLOR"
+pip install --user $APPPIPS --upgrade

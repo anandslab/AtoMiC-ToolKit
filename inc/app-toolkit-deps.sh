552682ba13dc6caffc9f908e835403c67a9bd931
TommyE123 <Tom.Eley@gmail.com>
2017-04-07 22:28:25 +0100

    Updated toolkit-deps

--- inc/app-toolkit-deps.sh
+++ inc/app-toolkit-deps.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo -e "$YELLOW--->CHECKING TOOLKIT DEPENDENCIES ...$ENDCOLOR"
-sudo apt-get -qq install apt-transport-https build-essential checkinstall curl \
-dnsutils git mediainfo mediainfo-gui p7zip-full par2 sed \
+sudo apt-get -qq install apt-transport-https build-essential \
+ca-certificates checkinstall curl dnsutils git lsb-release \
+mediainfo mediainfo-gui p7zip-full par2 sed \
 software-properties-common sqlite3 tar unzip wget zip -y
 
 echo

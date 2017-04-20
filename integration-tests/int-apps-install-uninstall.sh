1622be15ab064b1bfd12ccf8c730778984eadf41
TommyE123 <Tom.Eley@gmail.com>
2017-04-20 21:17:53 +0100

    Started the process to install and uninstall on Travis CI
    Currently trying to find what does and doesnt work installing

---
+++ integration-tests/int-apps-install-uninstall.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# shellcheck disable=SC2034
+# shellcheck disable=SC1090
+# Script Name: AtoMiC Integration Test Application Installer
+# Author: TommyE123
+# Publisher: http://www.htpcBeginner.com
+# License: MIT License (refer to README.md for more details)
+
+# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
+CI='YES'
+SCRIPTPATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+"$SCRIPTPATH/inc/app-toolkit-deps.sh"
+
+if [[ ! -d $SCRIPTPATH/tmp ]]; then
+    mkdir "$SCRIPTPATH/tmp"
+fi
+
+if [[ ! -f $SCRIPTPATH/tmp/userinfo ]]; then
+    echo "UNAME=$SUDO_USER" > "$SCRIPTPATH/tmp/userinfo"
+    echo "UGROUP=$SUDO_USER" >> "$SCRIPTPATH/tmp/userinfo"
+fi
+
+if [[ ! -f $SCRIPTPATH/tmp/consented ]]; then
+    echo "Agreed to disclaimers: $(date)" > "$SCRIPTPATH/tmp/consented"
+fi
+
+# source "$SCRIPTPATH/setup.sh" -i couchpotato
+# source "$SCRIPTPATH/setup.sh" -i deluged
+source "$SCRIPTPATH/setup.sh" -i emby-server
+source "$SCRIPTPATH/setup.sh" -u emby-server
+# source "$SCRIPTPATH/setup.sh" -i headphones
+# source "$SCRIPTPATH/setup.sh" -i htpcmanager
+# source "$SCRIPTPATH/setup.sh" -i jackett
+# source "$SCRIPTPATH/setup.sh" -i kodi
+# source "$SCRIPTPATH/setup.sh" -i lazylibrarian
+# source "$SCRIPTPATH/setup.sh" -i madsonic
+# source "$SCRIPTPATH/setup.sh" -i maximux
+# source "$SCRIPTPATH/setup.sh" -i mylar
+# source "$SCRIPTPATH/setup.sh" -i nzbget
+# source "$SCRIPTPATH/setup.sh" -i nzbhydra
+# source "$SCRIPTPATH/setup.sh" -i ombi
+# source "$SCRIPTPATH/setup.sh" -i plexmediaserver
+# source "$SCRIPTPATH/setup.sh" -i plexpy
+# source "$SCRIPTPATH/setup.sh" -i pyload
+# source "$SCRIPTPATH/setup.sh" -i qbittorrent-nox
+# source "$SCRIPTPATH/setup.sh" -i radarr
+# source "$SCRIPTPATH/setup.sh" -i sabnzbdplus
+# source "$SCRIPTPATH/setup.sh" -i sickgear
+# source "$SCRIPTPATH/setup.sh" -i sickrage
+# source "$SCRIPTPATH/setup.sh" -i sonarr
+# source "$SCRIPTPATH/setup.sh" -i subsonic
+# source "$SCRIPTPATH/setup.sh" -i transmission-daemon
+# source "$SCRIPTPATH/setup.sh" -i ubooquity
+# source "$SCRIPTPATH/setup.sh" -i watcher
+# source "$SCRIPTPATH/setup.sh" -i webmin

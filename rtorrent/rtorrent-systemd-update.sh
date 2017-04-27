be6c7db79bdde8abfd8d0ea06d38ca6eda813431
TommyE123 <Tom.Eley@gmail.com>
2017-04-27 21:37:17 +0100

    rTorrent cosmetic update

--- rtorrent/rtorrent-systemd-update.sh
+++ rtorrent/rtorrent-systemd-update.sh
@@ -6,8 +6,12 @@
 
 # DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
 
+echo -e "${YELLOW}--->Updating $APPTITLE systemd file to run as $CYAN$UNAME$ENDCOLOR...$ENDCOLOR"
+
 sudo sed -i "s@User=ReplaceMe@User=$UNAME@g" "/etc/systemd/system/$APPSYSTEMD" \
 || { echo -e "${RED}Modifying USER in SYSTEMD file failed.$ENDCOLOR"; exit 1; }
 
 sudo systemctl daemon-reload
 sudo systemctl enable "$APPSYSTEMD"
+
+echo 'OK'

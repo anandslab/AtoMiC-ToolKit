0b25622f9aba249e4c0e22419916d91f6ad053de
Tom <TommyE123@users.noreply.github.com>
2017-04-29 08:59:50 +0100

    ruTorrent - Added ffmpeg and Unrar to the install

--- rutorrent/rutorrent-installer.sh
+++ rutorrent/rutorrent-installer.sh
@@ -13,6 +13,8 @@ source "$SCRIPTPATH/inc/header.sh"
 echo -e "${GREEN}AtoMiC $APPTITLE Installer Script$ENDCOLOR"
 source "$SCRIPTPATH/inc/pause.sh"
 source "$SCRIPTPATH/utils/php/php-installer.sh"
+source "$SCRIPTPATH/utils/ffmpeg/ffmpeg-installer.sh"
+source "$SCRIPTPATH/utils/unrar/unrar-installer.sh"
 source "$SCRIPTPATH/rtorrent/rtorrent-installer.sh"
 source "$SCRIPTPATH/utils/nginx/nginx-installer.sh"
 source "$SCRIPTPATH/inc/app-stop.sh" #Stops NGINX

0bf04cd720107c4ff32ac8c5f69baaa6c188e54b
Tom <tom.eley@gmail.com>
2017-05-01 13:47:02 +0100

    Nginx - now uses 127.0.0.1

--- rutorrent/rutorrent-settings-configurator.sh
+++ rutorrent/rutorrent-settings-configurator.sh
@@ -15,7 +15,3 @@ echo "Set the correct folder permissions"
 #Set curl location
 sudo sed -i "s@\"curl\"	=> '',@\"curl\"	=> '/usr/bin/curl',@g" "$APPSETTINGS" \
 || { echo -e "${RED}Modifying CURL LOCATION in $APPSETTINGS file failed.$ENDCOLOR"; exit 1; }
-
-sudo sed -i "s@scgi_host = \"127.0.0.1\";@scgi_host = \"0.0.0.0\";@g" "$APPSETTINGS" \
-|| { echo -e "${RED}Modifying SCGI HOST LOCATION in $APPSETTINGS file failed.$ENDCOLOR"; exit 1; }
-

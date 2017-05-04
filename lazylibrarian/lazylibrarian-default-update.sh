752cb62a13bc497d19d5ecbd3d55673211c5e43f
Tom <tom.eley@gmail.com>
2017-05-04 20:10:26 +0100

    Lazy Librarian Fixes

--- lazylibrarian/lazylibrarian-default-update.sh
+++ lazylibrarian/lazylibrarian-default-update.sh
@@ -7,11 +7,8 @@
 
 echo -e "${YELLOW}--->Updating Default file...$ENDCOLOR"
 
-# [optional] change to 1 to enable daemon
-sudo sed -i 's@ENABLE_DAEMON=0@ENABLE_DAEMON=1@g' /etc/default/$APPNAME || { echo -e $RED'Replacing ENABLE_DAEMON failed.'$ENDCOLOR ; exit 1; }
-
 # [required] user or uid of account to run the program as:
-sudo sed -i "s@RUN_AS=@RUN_AS=$UNAME@g" /etc/default/$APPNAME || { echo -e $RED'Replacing RUN_AS failed.'$ENDCOLOR ; exit 1; }
+sudo sed -i "s@RUN_AS=\$USER@RUN_AS=$UNAME@g" /etc/default/$APPNAME || { echo -e $RED'Replacing RUN_AS failed.'$ENDCOLOR ; exit 1; }
 
 # [optional] full path to the configuration file of your choice;
 sudo sed -i "s@CONFIG=@CONFIG=$APPSETTINGS@g" /etc/default/$APPNAME || { echo -e $RED'Replacing CONFIG failed.'$ENDCOLOR ; exit 1; }
--- lazylibrarian/lazylibrarian-systemd-update.sh
+++ lazylibrarian/lazylibrarian-systemd-update.sh
@@ -3,12 +3,8 @@
 # Author: TommyE123
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)
-#
 
 # DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
 
-sudo sed -i "s@ExecStart=/usr/bin/python /home/lazylibrarian/LazyLibrarian.py --daemon --config /home/lazylibrarian/lazylibrarian.ini --datadir /home/lazylibrarian/.lazylibrarian --nolaunch@ExecStart=/usr/bin/python $APPPATH/LazyLibrarian.py --daemon --config $APPPATH/lazylibrarian.ini --datadir $APPPATH/data --nolaunch@g" /etc/systemd/system/$APPSYSTEMD  || { echo -e $RED'Modifying ExecStart in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
-sudo sed -i "s@User=lazylibrarian@User=$UNAME@g" /etc/systemd/system/$APPSYSTEMD  || { echo -e $RED'Modifying USER in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
-sudo sed -i "s@Group=lazylibrarian@Group=$UGROUP@g" /etc/systemd/system/$APPSYSTEMD  || { echo -e $RED'Modifying GROUP in SYSTEMD file failed.'$ENDCOLOR; exit 1; }
 sudo systemctl daemon-reload
 sudo systemctl enable $APPSYSTEMD

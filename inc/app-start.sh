d904881dd1e5e867eec5bcb7956c07901fe0375b
Tom <tom.eley@gmail.com>
2017-06-21 20:12:08 +0100

    Minor update to start script
    Added sleep command to give it a chance to correctly detect if the service started.

--- inc/app-start.sh
+++ inc/app-start.sh
@@ -6,13 +6,17 @@ if IsSystemdSupported; then
     if  [[ -f /etc/systemd/system/$APPSYSTEMD ]] || \
         [[ -f /lib/systemd/system/$APPSYSTEMD ]]; then
         sudo systemctl start $APPSYSTEMD
-        echo "$APPSYSTEMD is now: " && systemctl is-active $APPSYSTEMD
+        echo "$APPSYSTEMD is now: "
+        sleep 5
+        systemctl is-active $APPSYSTEMD
     fi
 
     if  [[ -f /etc/systemd/system/$APPSYSTEMD2 ]] || \
         [[ -f /lib/systemd/system/$APPSYSTEMD2 ]]; then
         sudo systemctl start $APPSYSTEMD2
-        echo "$APPSYSTEMD2 is now: " && systemctl is-active $APPSYSTEMD2
+        echo "$APPSYSTEMD2 is now: " 
+        sleep 5
+        systemctl is-active $APPSYSTEMD2
     fi
     
 elif [[ -f /etc/init.d/$APPNAME ]]; then

342606c7b71028e90b737e3df8cdc3cff99cf683
Tom <tom.eley@gmail.com>
2017-08-14 21:17:13 +0100

    Systemd service folder create fix
    Only create the systemd service folder if systemd is supported by that distro.

--- inc/app-systemd-add.sh
+++ inc/app-systemd-add.sh
@@ -21,9 +21,18 @@ fi
 
 # Some apps require an override to prevent the systemd file getting overwritten
 if [[ ! -z $APPSYSTEMDOVERIDE ]]; then
-    sudo cp "$APPSYSTEMDOVERIDE" \
+
+    APPSYSTEMDOVERIDELOC="/etc/systemd/system/$APPNAME.service.d" 
+    if [[ ! -d $APPSYSTEMDOVERIDELOC ]]; then
+        sudo mkdir -p $APPSYSTEMDOVERIDELOC
+        sudo chown $UNAME:$UGROUP $APPSYSTEMDOVERIDELOC || { echo -e "${RED}Chown on $folder failed.$ENDCOLOR"; exit 1; }
+    fi   
+
+    sudo cp -R "$APPSYSTEMDOVERIDE" \
     "/etc/systemd/system/$APPNAME.service.d/override.conf" \
     || { echo -e "${RED}Creating systemd override file failed.$ENDCOLOR" ; exit 1; }
-    sudo chmod 644 "/etc/systemd/system/$APPNAME.service.d/override.conf"
+    
+    sudo chmod -R 755 $APPSYSTEMDOVERIDELOC || { echo -e "${RED}Chmod on $folder failed.$ENDCOLOR"; exit 1; }
+ 
     echo "$APPSYSTEMD SystemD override script added"
 fi

d750488b4c36fe1c8b55fa1cff5dd155299396fc
Tom <tom.eley@gmail.com>
2017-08-20 09:32:38 +0100

    Added dirmngr toolkit deps

--- inc/app-toolkit-deps.sh
+++ inc/app-toolkit-deps.sh
@@ -4,6 +4,5 @@ echo -e "$YELLOW--->CHECKING TOOLKIT DEPENDENCIES ...$ENDCOLOR"
 sudo apt-get -qq install apt-transport-https build-essential \
 ca-certificates checkinstall curl dnsutils git lsb-release \
 mediainfo mediainfo-gui p7zip-full par2 sed \
-software-properties-common sqlite3 tar unzip wget zip -y
-
+software-properties-common sqlite3 tar unzip wget zip dirmngr -y
 echo

d11251ae391a2fd6c312f8aa6fd19fd3799b03f0
Tom <tom.eley@gmail.com>
2017-08-29 10:38:10 +0100

    Sabnzb+ repo updates

--- sabnzbdplus/sabnzbdplus-repository-configurator.sh
+++ sabnzbdplus/sabnzbdplus-repository-configurator.sh
@@ -5,19 +5,20 @@
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)
 
-# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
-
 CODENAME=$(lsb_release -c -s)
 REPOPPA='NO'
-if [ "$CODENAME" = 'squeeze' ]; then
+if [[ $CODENAME = 'squeeze' ]]; then
 APPREPOSITORYLINK='deb http://ppa.launchpad.net/jcfp/ppa/ubuntu lucid main'
     REPOKEYSREQ='YES'
-elif [ "$CODENAME" = 'wheezy' ]; then
+elif [[ $CODENAME = 'wheezy' ]]; then
 APPREPOSITORYLINK='deb http://ppa.launchpad.net/jcfp/ppa/ubuntu precise main'
     REPOKEYSREQ='YES'
-elif [ "$CODENAME" = 'jessie' ]; then
+elif [[ $CODENAME = 'jessie' ]]; then
 APPREPOSITORYLINK='deb http://ppa.launchpad.net/jcfp/ppa/ubuntu trusty main'
     REPOKEYSREQ='YES'
+elif [[ $CODENAME = 'stretch' ]]; then
+APPREPOSITORYLINK='deb http://ppa.launchpad.net/jcfp/ppa/ubuntu xenial main'
+    REPOKEYSREQ='YES'    
 else
     REPOPPA='YES'
     APPREPOSITORYLINK='ppa:jcfp/ppa'

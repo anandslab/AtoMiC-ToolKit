c75c157511c91ac30ce307d718e445fcc75fe542
Tom <tom.eley@gmail.com>
2017-09-10 18:04:19 +0100

    Added rvm uninstaller

--- utils/rvm/rvm-menu.sh
+++ utils/rvm/rvm-menu.sh
@@ -5,6 +5,7 @@ SUBCHOICE=$(whiptail --title "AtoMiC Toolkit - Manage Ruby Version Manager (RVM)
 --menu "What would you like to do?" --backtitle "$BACKTITLE" \
 --fb --cancel-button "Exit" $LINES $COLUMNS "$NETLINES" \
 "Install\Update" "Latest version of Ruby Version Manager (RVM)" \
+"Uninstall" "Latest version of Ruby Version Manager (RVM)" \
 "Go Back" "to previous menu" 3>&1 1>&2 2>&3)
 
 exitstatus=$?
@@ -13,6 +14,8 @@ if [[ $exitstatus = 0 ]]; then
     case "$SUBCHOICE" in
         "Install\Update" )
             source "$SCRIPTPATH/utils/$APPNAME/$APPNAME-installer.sh" ;;
+        "Uninstall" )
+            source "$SCRIPTPATH/utils/$APPNAME/$APPNAME-uninstaller.sh" ;;
         "Go Back" )
             source "$SCRIPTPATH/utils/utils-menu.sh" ;;
         *) 

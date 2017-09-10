5ecff907cf6660767a45d34c4d21d982b5684529
Tom <tom.eley@gmail.com>
2017-09-10 18:02:10 +0100

    Added gnupg2 to toolkit deps

--- inc/app-toolkit-deps.sh
+++ inc/app-toolkit-deps.sh
@@ -3,6 +3,6 @@ echo
 echo -e "$YELLOW--->CHECKING TOOLKIT DEPENDENCIES ...$ENDCOLOR"
 sudo apt-get -qq install apt-transport-https build-essential \
 ca-certificates checkinstall curl dnsutils git lsb-release \
-mediainfo mediainfo-gui p7zip-full par2 sed \
-software-properties-common sqlite3 tar unzip wget zip dirmngr -y
+mediainfo mediainfo-gui p7zip-full par2 sed gnupg2 zip \
+software-properties-common sqlite3 tar unzip wget dirmngr -y
 echo

a4e9af7cd65ff0ac2275223ab82effca70eb5420
Tom <tom.eley@gmail.com>
2017-09-10 21:25:17 +0100

    gpg2 key fixes

--- inc/app-keys-add.sh
+++ inc/app-keys-add.sh
@@ -4,7 +4,7 @@ echo -e $YELLOW"--->Adding $APPTITLE Keys..."$ENDCOLOR
 if [[ -n $REPRECVKEYSASC ]]; then
     wget -qO - $REPRECVKEYSASC | sudo apt-key add -
 elif [[ -n $REPRECVKEYSGPG ]]; then
-    su -c "gpg --keyserver $REPKEYSERVER --recv-keys $REPRECVKEYS" $UNAME
+    gpg2 --recv-keys $REPRECVKEYS
 else
     sudo apt-key adv --keyserver $REPKEYSERVER --recv-keys $REPRECVKEYS
 fi
--- inc/app-keys-remove.sh
+++ inc/app-keys-remove.sh
@@ -2,7 +2,9 @@
 echo
 echo -e $YELLOW"--->Removing $APPTITLE Keys..."$ENDCOLOR
 
-if [[ -n $(apt-key list $REPRECVKEYSHORT) ]]; then
+if [[ -n $REPRECVKEYSGPG ]]; then
+    gpg2 --batch --delete-keys $REPRECVKEYS
+elif [[ -n $(apt-key list $REPRECVKEYSHORT) ]]; then
     sudo apt-key del $REPRECVKEYSHORT
 else
     echo "$APPTITLE key doesn't exist"

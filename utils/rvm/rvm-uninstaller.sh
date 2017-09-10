76f096eb755d6a087f77e03eedfdd2e255c84336
Tom <tom.eley@gmail.com>
2017-09-10 20:15:09 +0100

    RVM uninstaller fixes

--- utils/rvm/rvm-uninstaller.sh
+++ utils/rvm/rvm-uninstaller.sh
@@ -5,12 +5,10 @@
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)
 
-echo
 echo -e "${GREEN}AtoMiC RVM Installer Script$ENDCOLOR"
-source /etc/profile.d/rvm.sh
+source /home/$UNAME/.rvm/scripts/rvm
 source "$SCRIPTPATH/inc/app-keys-remove.sh"
 rvm --default use system
 echo "Set Ruby to use system version $VERSION"
-
 rvm remove ruby
 echo -e "${GREEN}---> $APPTITLE uninstallation complete.$ENDCOLOR"

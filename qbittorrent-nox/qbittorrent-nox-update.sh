331072b402cfe8217edc7d5418a736bdcb1458b9
Tom <tom.eley@gmail.com>
2017-09-18 20:28:43 +0100

    qbittorrent - cosmetic code tidy up

--- qbittorrent-nox/qbittorrent-nox-update.sh
+++ qbittorrent-nox/qbittorrent-nox-update.sh
@@ -4,14 +4,10 @@
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)
 
-# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
-
 source "$SCRIPTPATH/inc/app-setup-check.sh"
 source "$SCRIPTPATH/inc/commons.sh"
 source "$SCRIPTPATH/inc/header.sh"
-
 echo -e "${GREEN}AtoMiC $APPTITLE Update Script$ENDCOLOR"
-
 source "$SCRIPTPATH/inc/pause.sh"
 source "$SCRIPTPATH/inc/app-folder-check.sh"
 source "$SCRIPTPATH/inc/app-stop.sh"

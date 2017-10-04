2d46f631796c99a3665685cbb9285d9976714c72
Tom <tom.eley@gmail.com>
2017-10-04 21:15:44 +0100

    rtorrent init fix

--- rtorrent/rtorrent-init-update.sh
+++ rtorrent/rtorrent-init-update.sh
@@ -10,3 +10,8 @@ sudo sed -i \
         "s@user=\"ReplaceMe\"@user=\"$UNAME\"@g" \
         "/etc/init.d/$APPINITD" || \
         { echo -e "$RED Replacing RUN_AS failed.$ENDCOLOR" ; exit 1; }
+
+sudo sed -i \
+        "s@sessiondir=\"ReplaceMe\"@sessiondir=\"/home/$UNAME/Downloads/rtorrent/session\"@g" \
+        "/etc/init.d/$APPINITD" || \
+        { echo -e "$RED Replacing sessiondir failed.$ENDCOLOR" ; exit 1; }

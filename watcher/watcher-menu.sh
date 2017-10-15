558deb0e35ea51cee0234c4fc6f3505d7e5bc4e7
Tom <tom.eley@gmail.com>
2017-10-15 18:56:05 +0100

    Updated Watcher to use the new repo and python3

--- watcher/watcher-menu.sh
+++ watcher/watcher-menu.sh
@@ -5,16 +5,12 @@
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)
 
-# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
-
 source "$SCRIPTPATH/inc/app-setup-check.sh"
 SUBCHOICE=$(whiptail --title "AtoMiC Toolkit - Manage Watcher" \
 --menu "What would you like to do?" --backtitle "$BACKTITLE" \
 --fb --cancel-button "Exit" $LINES $COLUMNS "$NETLINES" \
 "Install" "Install Watcher" \
 "Uninstall" "Uninstall Watcher" \
-"Backup" "Backup Watcher settings" \
-"Restore" "Restore Watcher settings from a previous backup" \
 "Manual Update" "Manually update Watcher" \
 "Access Details" "View Watcher access details" \
 "Go Back" "Back to Main Menu" 3>&1 1>&2 2>&3)
@@ -25,8 +21,6 @@ if [[ $exitstatus = 0 ]]; then
     case "$SUBCHOICE" in
         "Install" ) source "$SCRIPTPATH/$APPNAME/$APPNAME-installer.sh" ;;
         "Uninstall" ) source "$SCRIPTPATH/$APPNAME/$APPNAME-uninstaller.sh" ;;
-        "Backup" ) source "$SCRIPTPATH/inc/app-backup-controller.sh" ;;
-        "Restore" ) source "$SCRIPTPATH/inc/app-restore-controller.sh" ;;
         "Manual Update" ) source "$SCRIPTPATH/$APPNAME/$APPNAME-update.sh" ;;
         "Access Details" ) source "$SCRIPTPATH/inc/app-access-details.sh" ;;
         "Go Back" ) source "$SCRIPTPATH/menus/menu-pvrs.sh" ;;

c9ea2f1a3b4d62f23a695ef367ce6bb458cf9316
Tom <tom.eley@gmail.com>
2017-10-19 20:43:50 +0100

    Added Privite Internet Access Client Installer
    This is for the desktop client only. I'll start working on a Headlless install very soon.

---
+++ utils/pia/pia-client-uninstaller.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# shellcheck disable=SC1090
+# Script Name: AtoMiC Private Internet Access Client Uninstaller
+# Author: TommyE123
+# Publisher: http://www.htpcBeginner.com
+# License: MIT License (refer to README.md for more details)
+
+echo -e "${GREEN}AtoMiC $APPTITLE Uninstaller Script$ENDCOLOR"
+source "$SCRIPTPATH/inc/app-setup-check.sh"
+source "$SCRIPTPATH/utils/pia/pia-client-kill.sh"
+source "$SCRIPTPATH/inc/app-file-del.sh"
+source "$SCRIPTPATH/inc/app-uninstall-confirmation.sh"
---
+++ utils/pia/pia-menu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# shellcheck disable=SC1090
+source "$SCRIPTPATH/inc/app-setup-check.sh"
+SUBCHOICE=$(whiptail --title "AtoMiC Toolkit - Manage Private Internet Access" \
+--menu "What would you like to do?" --backtitle "$BACKTITLE" \
+--fb --cancel-button "Exit" $LINES $COLUMNS "$NETLINES" \
+"Install\\Update Client" "Install DESKTOP ONLY client" \
+"Uninstall" "Remove DESKTOP ONLY client" \
+"Go Back" "to previous menu" 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [[ $exitstatus = 0 ]]; then
+    source "$SCRIPTPATH/utils/pia/pia-constants.sh"
+    case "$SUBCHOICE" in
+        "Install\\Update Client" )
+            source "$SCRIPTPATH/utils/pia/pia-client-installer.sh" ;;
+        "Uninstall" )
+            source "$SCRIPTPATH/utils/pia/pia-client-uninstaller.sh" ;;
+        "Go Back" )
+            source "$SCRIPTPATH/utils/utils-menu.sh" ;;
+        *)
+            source "$SCRIPTPATH/inc/invalid-option.sh" ;;
+    esac
+else
+    source "$SCRIPTPATH/inc/thankyou.sh"
+    echo
+    sleep 1
+    exit 1
+fi

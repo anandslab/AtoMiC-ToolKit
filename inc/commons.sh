216e785ade12b2b7bc72247eb128a421423c073f
Tom <tom.eley@gmail.com>
2017-10-22 19:08:24 +0100

    Changed text colours
    Made more friendly to white screens.

--- inc/commons.sh
+++ inc/commons.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # shellcheck disable=SC2034
-YELLOW='\e[93m'
-RED='\e[91m'
+YELLOW='\e[33m'
+RED='\e[31m'
 ENDCOLOR='\033[0m'
-CYAN='\e[96m'
-GREEN='\e[92m'
+CYAN='\e[34m'
+GREEN='\e[32m'
 BACKTITLE='We have worked several hundred hours on AtoMiC ToolKit. Please visit www.htpcbeginner.com and share or follow us to show your support.'
 LINES=26
 COLUMNS=78

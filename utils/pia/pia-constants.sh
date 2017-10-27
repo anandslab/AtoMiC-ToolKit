7ba1177119f8ef387ee52910c09208cf3cf5ab64
Tom <tom.eley@gmail.com>
2017-10-27 20:34:02 +0100

    PIA - Added APPSETTINGSDIR
    So it prompts to delete the folder on uninstall.

--- utils/pia/pia-constants.sh
+++ utils/pia/pia-constants.sh
@@ -4,3 +4,4 @@ APPTITLE='PIA'
 APPNAME='pia'
 APPPATH="/home/$UNAME/.pia_manager"
 APPSETTINGS="/home/$UNAME/.local/share/applications/pia_manager.desktop"
+APPSETTINGSDIR="/home/$UNAME/.config/Private Internet Access"

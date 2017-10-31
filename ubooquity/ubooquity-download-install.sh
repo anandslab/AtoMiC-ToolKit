defe718aa6f03e1df4550508f86e98cf3d982275
Tom <tom.eley@gmail.com>
2017-10-31 20:08:48 +0000

    Ubooquity - Cosmetic fixes

--- ubooquity/ubooquity-download-install.sh
+++ ubooquity/ubooquity-download-install.sh
@@ -4,14 +4,12 @@
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)
 
-# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
-
 cd $(mktemp -d) || exit
 
 echo -e "${YELLOW}--->Downloading files...$ENDCOLOR"
 sudo wget "http://vaemendis.net/ubooquity/service/download.php" -O ubooquity.zip
 
 echo -e "${YELLOW}--->Extracting files...$ENDCOLOR"
-sudo unzip -q ubooquity*.zip -d "$APPPATH"
+sudo unzip -qo ubooquity*.zip -d "$APPPATH"
 echo 'Done'
 cd "$SCRIPTPATH" || exit
--- ubooquity/ubooquity-systemd-update.sh
+++ ubooquity/ubooquity-systemd-update.sh
@@ -4,8 +4,6 @@
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)
 
-# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
-
 sudo sed -i "s@User=ReplaceMe@User=$UNAME@g" "/etc/systemd/system/$APPSYSTEMD" || { echo -e "${RED}Modifying USER in SYSTEMD file failed.$ENDCOLOR"; exit 1; }
 sudo systemctl daemon-reload
 sudo systemctl enable "$APPSYSTEMD"

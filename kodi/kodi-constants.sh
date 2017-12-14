d14addf1a5d9313dc618ed9fc78983982556dd82
Tom <tom.eley@gmail.com>
2017-12-14 20:44:38 +0000

    Kodi - Added missing app path location

--- kodi/kodi-constants.sh
+++ kodi/kodi-constants.sh
@@ -2,6 +2,7 @@
 # shellcheck disable=SC2034
 APPNAME='kodi'
 APPSHORTNAME='kodi'
+APPPATH='/usr/share/kodi'
 APPTITLE='Kodi'
 APPDEPS='sndiod kodi-pvr-mythtv kodi-pvr-vuplus kodi-pvr-vdr-vnsi kodi-pvr-njoy kodi-pvr-nextpvr kodi-pvr-mediaportal-tvserver kodi-pvr-tvheadend-hts kodi-pvr-dvbviewer kodi-pvr-argustv kodi-pvr-iptvsimple kodi-audioencoder-vorbis kodi-audioencoder-flac kodi-audioencoder-lame'
 APPSYSTEMD='kodi.service'

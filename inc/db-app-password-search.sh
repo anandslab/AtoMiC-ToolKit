241ca7a0aab4d942d7582c72a21a567504e1a9aa
Tom <tom.eley@gmail.com>
2017-12-21 19:13:41 +0000

    DB password search fixes

--- inc/db-app-password-search.sh
+++ inc/db-app-password-search.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Finds the password in a Datebase
 
-if [[ -n $DBPASSSEARCH ]] && [[ -n $APPSETTINGS ]]; then
-    UIPASS=$(echo "$DBPASSSEARCH" | sqlite3 "$APPSETTINGS")
+if [[ -n $DBPASSSEARCH ]] && [[ -n $APPSETTINGSDB ]]; then
+    UIPASS=$(echo "$DBPASSSEARCH" | sqlite3 "$APPSETTINGSDB")
     if [[ -n $UIPASS ]]; then
         UIPASS=$(echo "$UIPASS" | tr -dc '[:alnum:]')
     fi

1781ef5bd0d42fa9c50753efc8a0ab011a3516fe
Tom <tom.eley@gmail.com>
2017-12-30 09:08:38 +0000

    Added Netdata Update

---
+++ netdata/netdata-update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# shellcheck disable=SC1090
+# Script Name: AtoMiC NetData Updater
+# Author: TommyE123
+# Publisher: http://www.htpcBeginner.com
+# License: MIT License (refer to README.md for more details)
+
+source "$SCRIPTPATH/inc/app-setup-check.sh"
+source "$SCRIPTPATH/inc/commons.sh"
+source "$SCRIPTPATH/inc/header.sh"
+echo -e "${GREEN}AtoMiC $APPTITLE Update Script$ENDCOLOR"
+source "$SCRIPTPATH/inc/pause.sh"
+
+if DoesAppFolderExist; then
+    source "$SCRIPTPATH/inc/app-stop.sh"
+    source "$SCRIPTPATH/inc/pkgupdate.sh"
+    sudo bash "$APPPATH/netdata-updater.sh"
+    source "$SCRIPTPATH/inc/app-start.sh"
+    source "$SCRIPTPATH/inc/app-update-confirmation.sh"
+    source "$SCRIPTPATH/inc/thankyou.sh"
+fi
+source "$SCRIPTPATH/inc/exit.sh"
+

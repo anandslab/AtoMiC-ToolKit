239bc8db06509edfc189bcfd930252250c370c92
Tom <tom.eley@gmail.com>
2017-12-31 11:27:44 +0000

    Service Start stop improvements

--- inc/app-init-add.sh
+++ inc/app-init-add.sh
@@ -4,7 +4,6 @@ sleep 1
 echo -e $YELLOW"--->Enabling $APPTITLE init.d startup script..."$ENDCOLOR
 if [[ ! -z $APPINITDLOC ]]; then
     sudo cp $APPINITDLOC /etc/init.d/$APPINITD || { echo -e $RED'Creating init file failed.'$ENDCOLOR ; exit 1; }
-    sudo chmod +x /etc/init.d/$APPINITD
     sudo update-rc.d "$APPINITD" defaults
     echo "/etc/init.d/$APPINITD script added and enabled"
 else
--- inc/app-start.sh
+++ inc/app-start.sh
@@ -20,23 +20,25 @@ if IsSystemdSupported; then
         fi
     fi
 
-    if  [[ -f /etc/systemd/system/$APPSYSTEMD2 ]] || \
-        [[ -f /lib/systemd/system/$APPSYSTEMD2 ]] || \
-        [[ -f /usr/lib/systemd/system/$APPSYSTEMD2 ]] || \
-        [[ -L /etc/systemd/system/multi-user.target.wants/$APPSYSTEMD2 ]]; then
-        FOUND=1
-        sudo systemctl start "$APPSYSTEMD2"
-        sleep 5
-        STATUS=$(systemctl is-active "$APPSYSTEMD2")
-        if [[ $STATUS = 'active' ]]; then
-            echo -e "$APPSYSTEMD2 is now: ${GREEN}$STATUS$ENDCOLOR"
-        else
-            echo -e "$APPSYSTEMD2 is now: ${RED}$STATUS$ENDCOLOR"
-            exit 1
+    if [[ -n $APPSYSTEMD2 ]]; then
+        if  [[ -f /etc/systemd/system/$APPSYSTEMD2 ]] || \
+            [[ -f /lib/systemd/system/$APPSYSTEMD2 ]] || \
+            [[ -f /usr/lib/systemd/system/$APPSYSTEMD2 ]] || \
+            [[ -L /etc/systemd/system/multi-user.target.wants/$APPSYSTEMD2 ]]; then
+            FOUND=2
+            sudo systemctl start "$APPSYSTEMD2"
+            sleep 5
+            STATUS=$(systemctl is-active "$APPSYSTEMD2")
+            if [[ $STATUS = 'active' ]]; then
+                echo -e "$APPSYSTEMD2 is now: ${GREEN}$STATUS$ENDCOLOR"
+            else
+                echo -e "$APPSYSTEMD2 is now: ${RED}$STATUS$ENDCOLOR"
+                exit 1
+            fi
         fi
     fi
 elif [[ -f /etc/init.d/$APPINITD ]]; then
-    FOUND=1
+    FOUND=3
     sudo service "$APPINITD" start
     sleep 5
 
@@ -48,6 +50,6 @@ elif [[ -f /etc/init.d/$APPINITD ]]; then
     fi
 fi
 
-if [[ -n $FOUND ]] && [[ $FOUND = 0 ]]; then
+if [[ -z $FOUND ]]; then
     echo -e "${RED}Startup file not found.$ENDCOLOR"
 fi

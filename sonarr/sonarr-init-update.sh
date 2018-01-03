61bec69b810490e4d7bd1b29e80627a74860bdc5
Tom <tom.eley@gmail.com>
2018-01-03 21:04:38 +0000

    Sonarr - Updated Init.d Script
    Now uses a generic script and correctly creates a PID file.

--- sonarr/sonarr-init-update.sh
+++ sonarr/sonarr-init-update.sh
@@ -2,10 +2,11 @@
 # Script Name: AtoMiC Sonarr init.d update
 # Author: TommyE123
 # Publisher: http://www.htpcbeginner.com
-#
 
-# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
 echo 'Updating init.d file'
 
-sudo sed -i "s@RUNASUSER=root@RUNASUSER=$UNAME@g" /etc/init.d/$APPINITD || { echo -e $RED'Replacing RUN_AS failed.'$ENDCOLOR ; exit 1; }
-sudo sed -i "s@RUNASGROUP=root@RUNASGROUP=$UGROUP@g" /etc/init.d/$APPINITD || { echo -e $RED'Replacing RUNASGROUP failed.'$ENDCOLOR ; exit 1; }
+sudo sed -i "s@user=\"ReplaceMe\"@user=\"$UNAME\"@g" /etc/init.d/$APPINITD \
+|| { echo -e "${RED}Replacing user failed.$ENDCOLOR" ; exit 1; }
+
+sudo sed -i "s@group=\"ReplaceMe\"@group=\"$UGROUP\"@g" /etc/init.d/$APPINITD \
+|| { echo -e "${RED}Replacing group failed.$ENDCOLOR" ; exit 1; }

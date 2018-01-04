f4cce6772d0d7a8dcd877de6e98ea6a5412aa80e
Tom <tom.eley@gmail.com>
2018-01-04 21:22:48 +0000

    Added ignore case to detect running init scripts

--- inc/app-stop.sh
+++ inc/app-stop.sh
@@ -36,7 +36,7 @@ if IsSystemdSupported; then
 elif [[ -f /etc/init.d/$APPINITD ]]; then
     FOUND=3
     sudo service $APPINITD stop
-    if [[ $(ps -ef | grep -v grep | grep -c "$APPINITD" ) == 0 ]]; then
+    if [[ $(ps -ef | grep -v grep | grep -ci "$APPINITD" ) == 0 ]]; then
         echo -e "$APPINITD is now: ${RED}stopped$ENDCOLOR"
     fi
 fi

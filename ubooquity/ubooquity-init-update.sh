50f39d7bc6f5ef04f8a822da3361fa1b5462abc3
Tom <tom.eley@gmail.com>
2018-01-04 20:29:35 +0000

    Ubooquity init fixes
    Changed to use a more generic template

--- ubooquity/ubooquity-init-update.sh
+++ ubooquity/ubooquity-init-update.sh
@@ -3,7 +3,9 @@
 # Author: TommyE123
 # Publisher: http://www.htpcbeginner.com
 
-if [[ -f /etc/init.d/$APPINITD ]]; then
-    echo 'Updating init.d file'
-    sudo sed -i "s@DAEMON_USER=ReplaceMe@DAEMON_USER=$UNAME@g" /etc/init.d/ubooquity || { echo -e "${RED}Replacing daemon username in init failed.$ENDCOLOR" ; exit 1; }
-fi
+echo 'Updating init.d file'
+sudo sed -i "s@user=\"ReplaceMe\"@user=\"$UNAME\"@g" /etc/init.d/$APPINITD \
+|| { echo -e "${RED}Replacing user failed.$ENDCOLOR" ; exit 1; }
+
+sudo sed -i "s@group=\"ReplaceMe\"@group=\"$UGROUP\"@g" /etc/init.d/$APPINITD \
+|| { echo -e "${RED}Replacing group failed.$ENDCOLOR" ; exit 1; }

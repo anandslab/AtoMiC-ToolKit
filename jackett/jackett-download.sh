0b8c7a992e69edf9c7ed30873ab861213d659271
Tom <tom.eley@gmail.com>
2018-01-06 19:17:26 +0000

    Jackett only updates if there is a newer version.

--- jackett/jackett-download.sh
+++ jackett/jackett-download.sh
@@ -3,16 +3,16 @@
 # Author: TommyE123
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)
-#
 
-# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
 echo
 echo -e "$YELLOW--->Latest File Found...$ENDCOLOR"
-OUTPUT="$(curl -s https://api.github.com/repos/jackett/jackett/releases | \
-        grep browser_download_url | \
-        head -n 1 | \
-        cut -d '"' -f 4)"
-echo "${OUTPUT}"
-echo
-echo -e "$YELLOW--->Downloading and extracting files...$ENDCOLOR"
-sudo curl -L "${OUTPUT}" | tar -xzf - -C "$APPPATH" --strip-components=1
+if "$SCRIPTPATH/jackett/jackett-version-handler.sh"; then
+    OUTPUT="$(curl -s https://api.github.com/repos/jackett/jackett/releases | \
+    grep browser_download_url | \
+    head -n 1 | \
+    cut -d '"' -f 4)"
+    echo "${OUTPUT}"
+    echo
+    echo -e "$YELLOW--->Downloading and extracting files...$ENDCOLOR"
+    sudo curl -L "${OUTPUT}" | tar -xzf - -C "$APPPATH" --strip-components=1
+fi
---
+++ jackett/jackett-version-handler.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# shellcheck disable=SC1090
+# Script Name: AtoMiC Jackett Version Check
+# Author: TommyE123
+# Publisher: http://www.htpcBeginner.com
+# License: MIT License (refer to README.md for more details)
+JACKETTVERSION=$(curl -s https://github.com/Jackett/Jackett/releases/latest  | grep -o '".*"' | awk -F / '{print $NF}' | sed s'/[v"]//g')
+echo "Available Version: $JACKETTVERSION"
+
+if ! "$SCRIPTPATH/inc/app-version-check.sh" "JACKETT" "$JACKETTVERSION" ; then
+    exit 1
+fi

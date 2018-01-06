e013425c1d77ec238d6e9ef7dd0213085c652800
Tom <tom.eley@gmail.com>
2018-01-06 10:42:15 +0000

    Glances - re-add systemd on update
    systemd file had a mistake copied from the offical site.
    Will remove when everyone has had a chance to update.

--- glances/glances-update.sh
+++ glances/glances-update.sh
@@ -12,8 +12,10 @@ echo -e "${GREEN}AtoMiC $APPTITLE Installer Script$ENDCOLOR"
 source "$SCRIPTPATH/inc/pause.sh"
 if DoesAppFolderExist; then
     source "$SCRIPTPATH/inc/app-stop.sh"
+    source "$SCRIPTPATH/inc/app-autostart-remove.sh" #TODO Remove when users have had time to update
     source "$SCRIPTPATH/inc/app-install-deps.sh"
     source "$SCRIPTPATH/inc/app-install-pips.sh"
+    source "$SCRIPTPATH/inc/app-autostart-configure.sh" #TODO Remove when users have had time to update
     source "$SCRIPTPATH/inc/app-start.sh"
     source "$SCRIPTPATH/inc/app-install-confirmation.sh"
     source "$SCRIPTPATH/inc/thankyou.sh"

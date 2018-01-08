7da19d7e5eef52e5c6c8225795c4d1e707a75df1
Tom <tom.eley@gmail.com>
2018-01-08 19:14:05 +0000

    Git update ignores permission changes

--- inc/app-git-download.sh
+++ inc/app-git-download.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
 echo
 sleep 1
 echo -e $YELLOW"--->Downloading latest $APPTITLE..."$ENDCOLOR
 echo -e 'Getting: '$CYAN$APPGIT$ENDCOLOR
 git clone --depth 1 $APPGIT $APPPATH || { echo -e $RED'Git not found.'$ENDCOLOR ; exit 1; }
+cd $APPPATH
+git config core.filemode false
 source "$SCRIPTPATH/inc/app-set-permissions.sh"
--- inc/app-git-update.sh
+++ inc/app-git-update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo
 sleep 1
-echo -e $YELLOW'--->Manually updating '$APPTITLE'...'$ENDCOLOR
+echo -e "${YELLOW}--->Manually updating $APPTITLE...$ENDCOLOR"
 cd $APPPATH
+git config core.filemode false
 git fetch --all
 git pull

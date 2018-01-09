d7fe1cca1223c57b99762d1361f047d46864cf96
Tom <tom.eley@gmail.com>
2018-01-09 19:06:55 +0000

    Added Newline errors to CI Tests
    Detects if there is missing Newlines returns at the bottom of every script.

---
+++ integration-tests/int-newline-errors.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script Name: AtoMiC Integration Test bashate check
+# Author: TommyE123
+# Publisher: http://www.htpcBeginner.com
+# License: MIT License (refer to README.md for more details)
+
+# Find double New Lines at the end of files
+if [[ $(find . -type f -exec sh -c '[ -z "$(sed -n "\$p" "$1")" ]' _ {} \; -print | wc -l) -ge 0 ]]; then
+    find . -type f -exec sh -c '[ -z "$(sed -n "\$p" "$1")" ]' _ {} \; -print
+    FOUND=1
+fi
+
+# Find missing New Lines
+if [[ $(pcregrep --exclude_dir='.git|backups' -LMr '\n\Z' . | wc -l) -ge 0 ]]; then
+    pcregrep --exclude_dir='.git|backups' -LMr '\n\Z' .
+    FOUND=1
+fi
+
+if [[ -z $FOUND ]]; then
+    exit 1
+fi

fe6d159162927b1dd962ca7d75f69dda063b116f
Tom <tom.eley@gmail.com>
2018-01-09 20:03:46 +0000

    Fix for newline errors

--- integration-tests/int-newline-errors.sh
+++ integration-tests/int-newline-errors.sh
@@ -5,19 +5,19 @@
 # License: MIT License (refer to README.md for more details)
 
 # Find double New Lines at the end of files
-if [[ $(find . -type f -exec sh -c '[ -z "$(sed -n "\$p" "$1")" ]' _ {} \; -print | wc -l) -ge 0 ]]; then
+if [[ $(find . -type f -exec sh -c '[ -z "$(sed -n "\$p" "$1")" ]' _ {} \; -print | wc -l) -gt 0 ]]; then
     find . -type f -exec sh -c '[ -z "$(sed -n "\$p" "$1")" ]' _ {} \; -print
+    echo "Test1"
     FOUND=1
 fi
 
 # Find missing New Lines
-if [[ $(pcregrep --exclude_dir='.git|backups' -LMr '\n\Z' . | wc -l) -ge 0 ]]; then
+if [[ $(pcregrep --exclude_dir='.git|backups' -LMr '\n\Z' . | wc -l) -gt 0 ]]; then
     pcregrep --exclude_dir='.git|backups' -LMr '\n\Z' .
+        echo "Test2"
     FOUND=1
 fi
 
 if [[ -n $FOUND ]]; then
     exit 1
-else
-    exit 0
 fi

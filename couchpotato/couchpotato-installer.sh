879eb1f19ee774e5ae55c03bac4991c0325da53f
Tom <tom.eley@gmail.com>
2018-01-14 21:10:30 +0000

    Now uses the new Python installer

--- couchpotato/couchpotato-installer.sh
+++ couchpotato/couchpotato-installer.sh
@@ -5,18 +5,16 @@
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)
 
-# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
-
 source "$SCRIPTPATH/inc/app-setup-check.sh"
 source "$SCRIPTPATH/inc/commons.sh"
 source "$SCRIPTPATH/inc/header.sh"
 echo -e "${GREEN}AtoMiC $APPTITLE Installer Script$ENDCOLOR"
 source "$SCRIPTPATH/inc/pause.sh"
 source "$SCRIPTPATH/inc/app-autostart-remove.sh"
-source "$SCRIPTPATH/inc/pkgupdate.sh"
-source "$SCRIPTPATH/inc/app-install-deps.sh"
-source "$SCRIPTPATH/inc/app-install-pips.sh"
 source "$SCRIPTPATH/inc/app-move-previous.sh"
+source "$SCRIPTPATH/utils/python/python-installer.sh"
+source "$SCRIPTPATH/couchpotato/couchpotato-constants.sh"
+source "$SCRIPTPATH/inc/app-install-deps.sh"
 source "$SCRIPTPATH/inc/app-git-download.sh"
 source "$SCRIPTPATH/inc/app-autostart-configure.sh"
 source "$SCRIPTPATH/inc/app-set-permissions.sh"

25030a3a89ba524aab3f99e40205f2f3ee15fa5e
Tom <tom.eley@gmail.com>
2018-01-23 18:09:35 +0000

    Python Pip - Fix bashate error

--- utils/python/python-pip-update.sh
+++ utils/python/python-pip-update.sh
@@ -17,7 +17,7 @@ fi
 
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $APPNAME | grep "install ok installed")
 if [[ -z $PKG_OK ]]; then
-  sudo apt-get -y install $APPNAME
+    sudo apt-get -y install $APPNAME
 fi
 
 sudo -H $1 freeze --local | grep -v '^\e' | cut -d = -f 1 | xargs -n1 sudo -H $1 install -U

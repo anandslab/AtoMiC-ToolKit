8488c3176f51ce5844b111951d2be0cb2ca10928
Tom <tom.eley@gmail.com>
2018-01-23 18:31:33 +0000

    Fixes for Utils apps

--- inc/app-access.sh
+++ inc/app-access.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
-if grep -q "Access Details" "$SCRIPTPATH/$APPNAME/$APPNAME-menu.sh"; then
-    ACCESSSUPPORT="For access details, run the $APPTITLE Access Details utility."
-else
-    ACCESSSUPPORT="You may now access $APPTITLE."
+if [[ -f "$SCRIPTPATH/$APPNAME/$APPNAME-menu.sh" ]]; then
+    if grep -q "Access Details" "$SCRIPTPATH/$APPNAME/$APPNAME-menu.sh"; then
+        ACCESSSUPPORT="For access details, run the $APPTITLE Access Details utility."
+    else
+        ACCESSSUPPORT="You may now access $APPTITLE."
+    fi
 fi
-echo -e "$ACCESSSUPPORT"
+echo "$ACCESSSUPPORT"

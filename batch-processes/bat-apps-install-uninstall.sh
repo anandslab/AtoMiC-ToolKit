1e333900069fc16d2ae978be7de6e597f2bbf47d
Tom <tom.eley@gmail.com>
2018-02-08 18:49:13 +0000

    Added Int Tests back in.

--- batch-processes/bat-apps-install-uninstall.sh
+++ batch-processes/bat-apps-install-uninstall.sh
@@ -6,94 +6,94 @@
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)
 
-# source "$SCRIPTPATH/setup.sh" -i couchpotato
-# source "$SCRIPTPATH/setup.sh" -x couchpotato
-# source "$SCRIPTPATH/setup.sh" -u couchpotato
-# source "$SCRIPTPATH/setup.sh" -i deluged
-# source "$SCRIPTPATH/setup.sh" -x deluged
-# source "$SCRIPTPATH/setup.sh" -u deluged
-# source "$SCRIPTPATH/setup.sh" -i emby-server
-# source "$SCRIPTPATH/setup.sh" -x emby-server
-# source "$SCRIPTPATH/setup.sh" -u emby-server
-# source "$SCRIPTPATH/setup.sh" -i glances
-# source "$SCRIPTPATH/setup.sh" -x glances
-# source "$SCRIPTPATH/setup.sh" -u glances
-# source "$SCRIPTPATH/setup.sh" -i grafana
-# source "$SCRIPTPATH/setup.sh" -x grafana
-# source "$SCRIPTPATH/setup.sh" -u grafana
-# source "$SCRIPTPATH/setup.sh" -i headphones
-# source "$SCRIPTPATH/setup.sh" -x headphones
-# source "$SCRIPTPATH/setup.sh" -u headphones
-# # source "$SCRIPTPATH/setup.sh" -i htpcmanager
-# # source "$SCRIPTPATH/setup.sh" -x htpcmanager
-# # source "$SCRIPTPATH/setup.sh" -u htpcmanager
-# source "$SCRIPTPATH/setup.sh" -i jackett
-# source "$SCRIPTPATH/setup.sh" -x jackett
-# source "$SCRIPTPATH/setup.sh" -u jackett
-# # source "$SCRIPTPATH/setup.sh" -i kodi
-# # source "$SCRIPTPATH/setup.sh" -u kodi
-# source "$SCRIPTPATH/setup.sh" -i lazylibrarian
-# source "$SCRIPTPATH/setup.sh" -x lazylibrarian
-# source "$SCRIPTPATH/setup.sh" -u lazylibrarian
-# source "$SCRIPTPATH/setup.sh" -i madsonic
-# source "$SCRIPTPATH/setup.sh" -x madsonic
-# source "$SCRIPTPATH/setup.sh" -u madsonic
-# source "$SCRIPTPATH/setup.sh" -i medusa
-# source "$SCRIPTPATH/setup.sh" -x medusa
-# source "$SCRIPTPATH/setup.sh" -u medusa
-# source "$SCRIPTPATH/setup.sh" -i muximux
-# source "$SCRIPTPATH/setup.sh" -u muximux
-# source "$SCRIPTPATH/setup.sh" -i mylar
-# source "$SCRIPTPATH/setup.sh" -x mylar
-# source "$SCRIPTPATH/setup.sh" -u mylar
-# source "$SCRIPTPATH/setup.sh" -i nzbget
-# source "$SCRIPTPATH/setup.sh" -x nzbget
-# source "$SCRIPTPATH/setup.sh" -u nzbget
-# # source "$SCRIPTPATH/setup.sh" -i netdata
-# # source "$SCRIPTPATH/setup.sh" -x netdata
-# # source "$SCRIPTPATH/setup.sh" -u netdata
-# source "$SCRIPTPATH/setup.sh" -i nzbhydra
-# source "$SCRIPTPATH/setup.sh" -x nzbhydra
-# source "$SCRIPTPATH/setup.sh" -u nzbhydra
-# source "$SCRIPTPATH/setup.sh" -i ombi
-# source "$SCRIPTPATH/setup.sh" -x ombi
-# source "$SCRIPTPATH/setup.sh" -u ombi
-# source "$SCRIPTPATH/setup.sh" -i organizr
-# source "$SCRIPTPATH/setup.sh" -u organizr
-# source "$SCRIPTPATH/setup.sh" -i phpsysinfo
-# source "$SCRIPTPATH/setup.sh" -u phpsysinfo
-# source "$SCRIPTPATH/setup.sh" -i plexmediaserver
-# source "$SCRIPTPATH/setup.sh" -x plexmediaserver
-# source "$SCRIPTPATH/setup.sh" -u plexmediaserver
-# source "$SCRIPTPATH/setup.sh" -i plexpy
-# source "$SCRIPTPATH/setup.sh" -x plexpy
-# source "$SCRIPTPATH/setup.sh" -u plexpy
-# source "$SCRIPTPATH/setup.sh" -i pyload
-# source "$SCRIPTPATH/setup.sh" -x pyload
-# source "$SCRIPTPATH/setup.sh" -u pyload
-# source "$SCRIPTPATH/setup.sh" -i qbittorrent-nox
-# source "$SCRIPTPATH/setup.sh" -x qbittorrent-nox
-# source "$SCRIPTPATH/setup.sh" -u qbittorrent-nox
-# source "$SCRIPTPATH/setup.sh" -i radarr
-# source "$SCRIPTPATH/setup.sh" -x radarr
-# source "$SCRIPTPATH/setup.sh" -u radarr
-# source "$SCRIPTPATH/setup.sh" -i rclone
-# source "$SCRIPTPATH/setup.sh" -u rclone
-# source "$SCRIPTPATH/setup.sh" -i rclone-browser
-# source "$SCRIPTPATH/setup.sh" -u rclone-browser
-# source "$SCRIPTPATH/setup.sh" -i resilio-sync
-# source "$SCRIPTPATH/setup.sh" -x resilio-sync
-# source "$SCRIPTPATH/setup.sh" -u resilio-sync
-# source "$SCRIPTPATH/setup.sh" -i rtorrent
-# source "$SCRIPTPATH/setup.sh" -u rtorrent
-# source "$SCRIPTPATH/setup.sh" -i rutorrent
-# source "$SCRIPTPATH/setup.sh" -u rutorrent
-# source "$SCRIPTPATH/setup.sh" -i sabnzbdplus
-# source "$SCRIPTPATH/setup.sh" -x sabnzbdplus
-# source "$SCRIPTPATH/setup.sh" -u sabnzbdplus
-# source "$SCRIPTPATH/setup.sh" -i sickgear
-# source "$SCRIPTPATH/setup.sh" -x sickgear
-# source "$SCRIPTPATH/setup.sh" -u sickgear
+source "$SCRIPTPATH/setup.sh" -i muximux
+source "$SCRIPTPATH/setup.sh" -u muximux
+source "$SCRIPTPATH/setup.sh" -i couchpotato
+source "$SCRIPTPATH/setup.sh" -x couchpotato
+source "$SCRIPTPATH/setup.sh" -u couchpotato
+source "$SCRIPTPATH/setup.sh" -i deluged
+source "$SCRIPTPATH/setup.sh" -x deluged
+source "$SCRIPTPATH/setup.sh" -u deluged
+source "$SCRIPTPATH/setup.sh" -i emby-server
+source "$SCRIPTPATH/setup.sh" -x emby-server
+source "$SCRIPTPATH/setup.sh" -u emby-server
+source "$SCRIPTPATH/setup.sh" -i glances
+source "$SCRIPTPATH/setup.sh" -x glances
+source "$SCRIPTPATH/setup.sh" -u glances
+source "$SCRIPTPATH/setup.sh" -i grafana
+source "$SCRIPTPATH/setup.sh" -x grafana
+source "$SCRIPTPATH/setup.sh" -u grafana
+source "$SCRIPTPATH/setup.sh" -i headphones
+source "$SCRIPTPATH/setup.sh" -x headphones
+source "$SCRIPTPATH/setup.sh" -u headphones
+# source "$SCRIPTPATH/setup.sh" -i htpcmanager
+# source "$SCRIPTPATH/setup.sh" -x htpcmanager
+# source "$SCRIPTPATH/setup.sh" -u htpcmanager
+source "$SCRIPTPATH/setup.sh" -i jackett
+source "$SCRIPTPATH/setup.sh" -x jackett
+source "$SCRIPTPATH/setup.sh" -u jackett
+# source "$SCRIPTPATH/setup.sh" -i kodi
+# source "$SCRIPTPATH/setup.sh" -u kodi
+source "$SCRIPTPATH/setup.sh" -i lazylibrarian
+source "$SCRIPTPATH/setup.sh" -x lazylibrarian
+source "$SCRIPTPATH/setup.sh" -u lazylibrarian
+source "$SCRIPTPATH/setup.sh" -i madsonic
+source "$SCRIPTPATH/setup.sh" -x madsonic
+source "$SCRIPTPATH/setup.sh" -u madsonic
+source "$SCRIPTPATH/setup.sh" -i medusa
+source "$SCRIPTPATH/setup.sh" -x medusa
+source "$SCRIPTPATH/setup.sh" -u medusa
+source "$SCRIPTPATH/setup.sh" -i mylar
+source "$SCRIPTPATH/setup.sh" -x mylar
+source "$SCRIPTPATH/setup.sh" -u mylar
+source "$SCRIPTPATH/setup.sh" -i nzbget
+source "$SCRIPTPATH/setup.sh" -x nzbget
+source "$SCRIPTPATH/setup.sh" -u nzbget
+# source "$SCRIPTPATH/setup.sh" -i netdata
+# source "$SCRIPTPATH/setup.sh" -x netdata
+# source "$SCRIPTPATH/setup.sh" -u netdata
+source "$SCRIPTPATH/setup.sh" -i nzbhydra
+source "$SCRIPTPATH/setup.sh" -x nzbhydra
+source "$SCRIPTPATH/setup.sh" -u nzbhydra
+source "$SCRIPTPATH/setup.sh" -i ombi
+source "$SCRIPTPATH/setup.sh" -x ombi
+source "$SCRIPTPATH/setup.sh" -u ombi
+source "$SCRIPTPATH/setup.sh" -i organizr
+source "$SCRIPTPATH/setup.sh" -u organizr
+source "$SCRIPTPATH/setup.sh" -i phpsysinfo
+source "$SCRIPTPATH/setup.sh" -u phpsysinfo
+source "$SCRIPTPATH/setup.sh" -i plexmediaserver
+source "$SCRIPTPATH/setup.sh" -x plexmediaserver
+source "$SCRIPTPATH/setup.sh" -u plexmediaserver
+source "$SCRIPTPATH/setup.sh" -i plexpy
+source "$SCRIPTPATH/setup.sh" -x plexpy
+source "$SCRIPTPATH/setup.sh" -u plexpy
+source "$SCRIPTPATH/setup.sh" -i pyload
+source "$SCRIPTPATH/setup.sh" -x pyload
+source "$SCRIPTPATH/setup.sh" -u pyload
+source "$SCRIPTPATH/setup.sh" -i qbittorrent-nox
+source "$SCRIPTPATH/setup.sh" -x qbittorrent-nox
+source "$SCRIPTPATH/setup.sh" -u qbittorrent-nox
+source "$SCRIPTPATH/setup.sh" -i radarr
+source "$SCRIPTPATH/setup.sh" -x radarr
+source "$SCRIPTPATH/setup.sh" -u radarr
+source "$SCRIPTPATH/setup.sh" -i rclone
+source "$SCRIPTPATH/setup.sh" -u rclone
+source "$SCRIPTPATH/setup.sh" -i rclone-browser
+source "$SCRIPTPATH/setup.sh" -u rclone-browser
+source "$SCRIPTPATH/setup.sh" -i resilio-sync
+source "$SCRIPTPATH/setup.sh" -x resilio-sync
+source "$SCRIPTPATH/setup.sh" -u resilio-sync
+source "$SCRIPTPATH/setup.sh" -i rtorrent
+source "$SCRIPTPATH/setup.sh" -u rtorrent
+source "$SCRIPTPATH/setup.sh" -i rutorrent
+source "$SCRIPTPATH/setup.sh" -u rutorrent
+source "$SCRIPTPATH/setup.sh" -i sabnzbdplus
+source "$SCRIPTPATH/setup.sh" -x sabnzbdplus
+source "$SCRIPTPATH/setup.sh" -u sabnzbdplus
+source "$SCRIPTPATH/setup.sh" -i sickgear
+source "$SCRIPTPATH/setup.sh" -x sickgear
+source "$SCRIPTPATH/setup.sh" -u sickgear
 source "$SCRIPTPATH/setup.sh" -i sickrage
 source "$SCRIPTPATH/setup.sh" -x sickrage
 source "$SCRIPTPATH/setup.sh" -u sickrage

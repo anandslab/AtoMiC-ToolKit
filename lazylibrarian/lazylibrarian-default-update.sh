26451c469438a61e63094c53de279947bcebdd32
Tom <TommyE123@users.noreply.github.com>
2018-02-14 19:31:49 +0000

    Fixed Lazy Librarian APP_PATH Change
    The path has been changed by the LL dev which has resulted in the install failing.

--- lazylibrarian/lazylibrarian-default-update.sh
+++ lazylibrarian/lazylibrarian-default-update.sh
@@ -3,17 +3,18 @@
 # Author: TommyE123
 # Publisher: http://www.htpcbeginner.com
 
-# DO NOT EDIT ANYTHING UNLESS YOU KNOW WHAT YOU ARE DOING.
-
 echo -e "${YELLOW}--->Updating Default file...$ENDCOLOR"
 
 # [required] user or uid of account to run the program as:
-sudo sed -i "s@RUN_AS=\$USER@RUN_AS=$UNAME@g" /etc/default/$APPNAME || { echo -e $RED'Replacing RUN_AS failed.'$ENDCOLOR ; exit 1; }
+sudo sed -i "s@RUN_AS=\$USER@RUN_AS=$UNAME@g" /etc/default/$APPNAME || { echo -e "${RED}Replacing RUN_AS failed.$ENDCOLOR" ; exit 1; }
+
+# [required] set path where lazylibrarian is installed:
+sudo sed -i "s@APP_PATH=/opt/LazyLibrarian@APP_PATH=$APPPATH@g" /etc/default/$APPNAME || { echo -e "${RED}Replacing APP_PATH failed.$ENDCOLOR" ; exit 1; }
 
 # [optional] full path to the configuration file of your choice;
-sudo sed -i "s@CONFIG=@CONFIG=$APPSETTINGS@g" /etc/default/$APPNAME || { echo -e $RED'Replacing CONFIG failed.'$ENDCOLOR ; exit 1; }
+sudo sed -i "s@CONFIG=@CONFIG=$APPSETTINGS@g" /etc/default/$APPNAME || { echo -e "${RED}Replacing CONFIG failed.$ENDCOLOR" ; exit 1; }
 
 # [optional] full path to the folder to store data (databases/thumbs) in;
-sudo sed -i "s@DATADIR=@DATADIR=$APPPATH/data@g" /etc/default/$APPNAME || { echo -e $RED'Replacing DATADIR failed.'$ENDCOLOR ; exit 1; }
+sudo sed -i "s@DATADIR=@DATADIR=$APPPATH/data@g" /etc/default/$APPNAME || { echo -e "${RED}Replacing DATADIR failed.$ENDCOLOR" ; exit 1; }
 
 echo 'Updated Default file with new values'

af03a9496fc9d70e488eba7719b6e235feac8f50
Tom <TommyE123@users.noreply.github.com>
2018-02-17 12:04:20 +0000

    Removed Ombi from tests

--- batch-processes/bat-apps-install-uninstall.sh
+++ batch-processes/bat-apps-install-uninstall.sh
@@ -55,9 +55,9 @@ source "$SCRIPTPATH/setup.sh" -u nzbget
 source "$SCRIPTPATH/setup.sh" -i nzbhydra
 source "$SCRIPTPATH/setup.sh" -x nzbhydra
 source "$SCRIPTPATH/setup.sh" -u nzbhydra
-source "$SCRIPTPATH/setup.sh" -i ombi
-source "$SCRIPTPATH/setup.sh" -x ombi
-source "$SCRIPTPATH/setup.sh" -u ombi
+# source "$SCRIPTPATH/setup.sh" -i ombi
+# source "$SCRIPTPATH/setup.sh" -x ombi
+# source "$SCRIPTPATH/setup.sh" -u ombi
 source "$SCRIPTPATH/setup.sh" -i organizr
 source "$SCRIPTPATH/setup.sh" -u organizr
 source "$SCRIPTPATH/setup.sh" -i phpsysinfo

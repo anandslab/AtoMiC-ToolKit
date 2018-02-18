e25faf999d31c1f5dc4d8ea7f9a3e77e9aac759d
Tom <tom.eley@gmail.com>
2018-02-18 19:33:38 +0000

    Subsonic Reverse Proxy enabled

---
+++ subsonic/subsonic-reverse-proxy-disable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Script Name: AtoMiC Subsonic Reverse Proxy Disable.
+# Author: TommyE123
+# Publisher: http://www.htpcBeginner.com
+# License: MIT License (refer to README.md for more details)
+
+if sed -i "/ExecStart=\\/usr\\/bin\\/subsonic/c\\ExecStart=\\/usr\\/bin\\/subsonic" "/etc/systemd/system/$APPSYSTEMD"; then
+    echo "Updated context-path in /etc/systemd/system/$APPSYSTEMD"
+fi
+
+sudo systemctl daemon-reload

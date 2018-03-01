7031244cfcfff47a830b71bd6e273635187ce2ee
Eric Nemchik <eric@nemchik.com>
2018-03-01 15:53:39 -0600

    Fix SC1001

--- jackett/jackett-version-handler.sh
+++ jackett/jackett-version-handler.sh
@@ -10,8 +10,8 @@ source "$SCRIPTPATH/inc/commons.sh"
 echo -e "${YELLOW}--->Jackett Version Check...$ENDCOLOR"
 JACKETTVERSION=$(curl -s https://github.com/Jackett/Jackett/releases/latest  | \
 grep -o '".*"' | \
-awk -F / '{print $NF}' |
-\sed s'/[v"]//g')
+awk -F / '{print $NF}' | \
+sed s'/[v"]//g')
 echo "Available Version: $JACKETTVERSION"
 
 if ! checkappversion "JACKETT" "$JACKETTVERSION" ; then

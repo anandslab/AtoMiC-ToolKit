08ca50d4032756723d5f0027f46c5c61a1a7207e
Eric Nemchik <eric@nemchik.com>
2018-03-02 09:24:22 -0600

    Slim down the shellcheck ignores so we fix the important ones

--- integration-tests/int-shellcheck.sh
+++ integration-tests/int-shellcheck.sh
@@ -4,7 +4,7 @@
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)
 
-find . -name '*.sh' -print0 | xargs -0 shellcheck -e SC1090 -e SC1117 -e SC2009 -e SC2016 -e SC2034 -e SC2053 -e SC2086 -eSC2128 -e SC2153 -e SC2154 -e SC2181 -e SC2206 -e SC2207 -e SC2216
+find . -name '*.sh' -print0 | xargs -0 shellcheck -e SC1017 -e SC1090 -e SC2034 -e SC2086 -e SC2215
 
 SCRIPTPATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 

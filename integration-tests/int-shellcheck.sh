fedff73a7d8ea0eb864c4b0c78f97b335207ebee
Tom <tom.eley@gmail.com>
2018-03-06 20:05:03 +0000

    ShellCheck errors now fail the build.

--- integration-tests/int-shellcheck.sh
+++ integration-tests/int-shellcheck.sh
@@ -4,14 +4,23 @@
 # Publisher: http://www.htpcBeginner.com
 # License: MIT License (refer to README.md for more details)
 
-find . -name '*.sh' -print0 | xargs -0 shellcheck -e SC1017 -e SC1090 -e SC2034 -e SC2086 -e SC2215
-
 SCRIPTPATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+source "$SCRIPTPATH/inc/commons.sh"
 
-numprocesses=$(grep -r '^# shellcheck disable' "$SCRIPTPATH" | grep -c 'shellcheck disable')
+shellcheck -V
 
-if [[ $numprocesses -gt 0 ]] ; then
-    echo "shellcheck disable warnings found"
+# -e SC1017 -e SC1090 -e SC2034 -e SC2086 -e SC2215
+NoShellCheckCodeWarningsFound=$(find . -name '*.sh' -print0 | xargs -0 shellcheck)
+if [[ -n $NoShellCheckCodeWarningsFound ]] ; then
+    echo -e "${RED}Shellcheck warnings found$ENDCOLOR"
+    find . -name '*.sh' -print0 | xargs -0 shellcheck
+    exit 1
+fi
+
+# Search for ShellCheck Warnings in all the scripts and fail if it finds any
+NoSCDISABLED=$(grep -r '^# shellcheck disable' "$SCRIPTPATH" | grep -c 'shellcheck disable')
+if [[ $NoSCDISABLED -gt 0 ]] ; then
+    echo -e "${RED}Shellcheck disable warnings found$ENDCOLOR"
     grep -rn "$SCRIPTPATH" -e '^# shellcheck disable'
     exit 1
 fi

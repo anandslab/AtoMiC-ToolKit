8155834c4dc7e44fdf3dd2af77ab72e77da5e0b3
Tom <TommyE123@users.noreply.github.com>
2018-03-16 21:04:14 +0000

    Transmission fixes (#45)
    * Transmission Startup fixes
    
    Added Type=simple to the override.conf due to the repo breaking the app starting. 
    Other general code cleanup.

--- transmission-daemon/transmission-daemon-default-update.sh
+++ transmission-daemon/transmission-daemon-default-update.sh
@@ -7,11 +7,12 @@ echo -e "${YELLOW}--->Updating Default file...$ENDCOLOR"
 # [optional] change to 1 to enable daemon
 if [[ -f /etc/default/$APPNAME ]]; then
     echo 'Updating default file'
-    sudo sed -i "s@/var/lib/transmission-daemon/info@$APPPATH@g" \
-                /etc/default/transmission-daemon || \
-                { echo -e \
-                "${RED}Replacing config directory in default failed.$ENDCOLOR"; \
-                exit 1; }
+    if sudo sed -i "s@/var/lib/transmission-daemon/info@$APPPATH@g" \
+            /etc/default/transmission-daemon || \
+            { echo -e "${RED}Replacing config directory in default failed.$ENDCOLOR"; \
+            exit 1; }; then
+        echo "Set CONFIG DIR to $APPPATH"
+    fi
 fi
 
 echo 'Updated Default file with new values'
--- transmission-daemon/transmission-daemon-init-update.sh
+++ transmission-daemon/transmission-daemon-init-update.sh
@@ -3,5 +3,8 @@
 
 if [[ -f /etc/init.d/$APPINITD ]]; then
     echo 'Updating init.d file'
-    sudo sed -i "s@USER=debian-transmission@USER=$UNAME@g" /etc/init.d/transmission-daemon || { echo -e $RED'Replacing daemon username in init failed.'$ENDCOLOR ; exit 1; }
+    if sudo sed -i "s@USER=debian-transmission@USER=$UNAME@g" /etc/init.d/transmission-daemon || \
+            { echo -e "${RED}Replacing daemon username in init.d failed.$ENDCOLOR" ; exit 1; }; then
+        echo "Set USER to $UNAME"
+    fi
 fi

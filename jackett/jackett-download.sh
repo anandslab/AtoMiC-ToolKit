23dc6f1ade92c3b7665d79849b000e0107740178
Tom <TommyE123@users.noreply.github.com>
2018-03-27 20:15:04 +0100

    Jackett download fixes

--- jackett/jackett-download.sh
+++ jackett/jackett-download.sh
@@ -4,10 +4,11 @@
 echo
 echo -e "$YELLOW--->Latest File Found...$ENDCOLOR"
 OUTPUT="$(curl -s https://api.github.com/repos/jackett/jackett/releases | \
+grep Mono.tar.gz | \
 grep browser_download_url | \
 head -n 1 | \
 cut -d '"' -f 4)"
 echo "${OUTPUT}"
 echo
 echo -e "$YELLOW--->Downloading and extracting files...$ENDCOLOR"
-sudo curl -L "${OUTPUT}" | tar -xzf - -C "$APPPATH" --strip-components=1
+curl -L "${OUTPUT}" | tar -xzf - -C "$APPPATH" --strip-components=1

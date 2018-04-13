27a87fcdb1077bb9eff6896455ca3ac2215aa6de
Tom <tom.eley@gmail.com>
2018-04-13 18:57:40 +0100

    Disabled LazyLibrarian in Travis-CI
    Init scripts are broken.

--- batch-processes/bat-apps-install-uninstall.sh
+++ batch-processes/bat-apps-install-uninstall.sh
@@ -38,9 +38,9 @@ source "$SCRIPTPATH/setup.sh" -u headphones
 # source "$SCRIPTPATH/setup.sh" -u jackett
 # source "$SCRIPTPATH/setup.sh" -i kodi
 # source "$SCRIPTPATH/setup.sh" -u kodi
-source "$SCRIPTPATH/setup.sh" -i lazylibrarian
+# source "$SCRIPTPATH/setup.sh" -i lazylibrarian # Init.d script broken
 # source "$SCRIPTPATH/setup.sh" -x lazylibrarian
-source "$SCRIPTPATH/setup.sh" -u lazylibrarian
+# source "$SCRIPTPATH/setup.sh" -u lazylibrarian
 source "$SCRIPTPATH/setup.sh" -i madsonic
 # source "$SCRIPTPATH/setup.sh" -x madsonic
 source "$SCRIPTPATH/setup.sh" -u madsonic
